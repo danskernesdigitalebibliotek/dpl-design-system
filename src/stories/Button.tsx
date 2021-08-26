6d9c754163a5115725a0bf6f634cbb647d17c915
matis-dk <mathiasp50@gmail.com>
2021-08-26 14:02:16 +0200

    Initialized CRA and Storybook

---
+++ src/stories/Button.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './button.css';
+
+interface ButtonProps {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = (props: ButtonProps) => {
+  const {
+    primary = false,
+    size = 'medium',
+    backgroundColor,
+    label,
+    ...extraProps
+  } = props;
+
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ' '
+      )}
+      style={{ backgroundColor }}
+      {...extraProps}
+    >
+      {label}
+    </button>
+  );
+};
