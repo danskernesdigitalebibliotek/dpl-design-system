806efaa4ec90a170969cd1dc18cbd0246409ec56
matis-dk <mathiasp50@gmail.com>
2021-09-08 16:13:53 +0200

    Added Accordion story

---
+++ src/stories/accordion/Accordion.stories.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { ComponentStory } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
+
+import { Accordion as AccordionComp } from "./Accordion";
+
+type AccordionProps = typeof AccordionComp;
+
+export default {
+  title: "DPL Design System / Accordion",
+  component: AccordionComp,
+  decorators: [withDesign],
+  design: {
+    type: "figma",
+    url:
+      "https://www.figma.com/file/Zx9GrkFA3l4ISvyZD2q0Qi/Designsystem?node-id=836%3A5757",
+  },
+};
+
+const Template: ComponentStory<AccordionProps> = (args) => (
+  <AccordionComp {...args} />
+);
+
+const list = ["Om Bibliotekerne", "Online tilbud", "Kontakt"].map((header) => ({
+  header,
+  content: [
+    {
+      title: "Brug bibliotekerne",
+      href: "/",
+    },
+    {
+      title: "Erstatninger og gebyrer",
+      href: "/",
+    },
+    {
+      title: "Opret bruger",
+      href: "/",
+    },
+    {
+      title: "Biblioteket - A til Å",
+      href: "/",
+    },
+    {
+      title: "Internetadgang",
+      href: "/",
+    },
+    {
+      title: "Print, scan og kopi",
+      href: "/",
+    },
+    {
+      title: "Booking af lokaler",
+      href: "/",
+    },
+    {
+      title: "Nyhedsbrev",
+      href: "/",
+    },
+  ],
+}));
+
+export const Accordion = Template.bind({});
+Accordion.args = {
+  list,
+};
---
+++ src/stories/accordion/accordion.js
@@ -0,0 +1,45 @@
+function initAccordion() {
+  // Checking if the listener is initialized
+  if (window.dpl?.accordion) {
+    return;
+  }
+
+  const accordionComponent = document.querySelectorAll(".accordion");
+  if (accordionComponent.length === 0) {
+    console.info("Accordion - Couldn't find class");
+    return;
+  }
+
+  function accordion(event) {
+    if (!event?.target?.closest) {
+      console.info("Accordion - No event target");
+      return;
+    }
+
+    const btn = event.target.closest("[data-accordion-trigger]");
+
+    if (!btn?.getAttribute) {
+      console.info("Accordion - Can't find accordion row");
+      return;
+    }
+
+    if (!btn.parentNode?.nextElementSibling) {
+      console.info("Accordion - Can't find next sibling too parentNode");
+      return;
+    }
+
+    const isOpen = btn.getAttribute("aria-expanded") === "true";
+    const target = btn.parentNode.nextElementSibling;
+
+    btn.setAttribute("aria-expanded", !isOpen);
+    target.hidden = isOpen;
+  }
+
+  accordionComponent.forEach((i) => i.addEventListener("click", accordion));
+  window.dpl = {
+    ...window.dpl,
+    accordion: true,
+  };
+}
+
+initAccordion();
