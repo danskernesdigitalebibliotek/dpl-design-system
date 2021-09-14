bbf48e35b1f3548088ca28e21e518a3733978a6d
matis-dk <mathiasp50@gmail.com>
2021-09-14 14:58:32 +0200

    Extended size control support for Pagefold story

--- src/stories/pagefold/Pagefold.tsx
+++ src/stories/pagefold/Pagefold.tsx
@@ -2,18 +2,26 @@ import React from "react";
 
 type PagefoldProps = {
   inherit: boolean;
+  container: boolean;
+  size: "small" | "medium" | "large" | "xlarge";
+  children?: React.ReactNode;
+  classes?: string;
+  compProps?: React.ComponentPropsWithoutRef<"div">;
 };
 
 export const Pagefold = (props: PagefoldProps) => {
-  const { inherit } = props;
+  const { inherit, container, size, children, classes, compProps } = props;
+  const containerClass = container ? "internal-pagefold-parent" : "";
+
+  const parent = `pagefold-parent-${size} ${containerClass} ${classes || ""}`;
+  const child = `pagefold-triangle-${size} ${
+    inherit ? "pagefold-inherit-parent" : ""
+  }`;
 
   return (
-    <div className="pagefold-parent internal-pagefold-parent">
-      <div
-        className={`pagefold-triangle ${
-          inherit ? "pagefold-inherit-parent" : ""
-        }`}
-      ></div>
+    <div className={parent} {...compProps}>
+      <div className={child} />
+      {children}
     </div>
   );
 };
