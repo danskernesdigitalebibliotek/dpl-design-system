83aa31c7ba45f3c825c0677ab65bd463219bb765
Pernille <pernille@reload.dk>
2021-09-22 15:19:11 +0200

    Refactor code after review

--- src/stories/footer/Footer.tsx
+++ src/stories/footer/Footer.tsx
@@ -3,48 +3,10 @@ import { Pagefold } from "../pagefold/Pagefold";
 import { Accordion } from "../accordion/Accordion";
 import { Dropdown } from "../dropdown/Dropdown";
 import { Logo } from "../logo/Logo";
+import { list } from "../accordion/accordionList";
+import { FooterColumn } from "./FooterColumn";
 
 export const Footer = () => {
-  const accordionList = ["Om Bibliotekerne", "Online tilbud", "Kontakt"].map(
-    (header) => ({
-      header,
-      content: [
-        {
-          title: "Brug bibliotekerne",
-          href: "/",
-        },
-        {
-          title: "Erstatninger og gebyrer",
-          href: "/",
-        },
-        {
-          title: "Opret bruger",
-          href: "/",
-        },
-        {
-          title: "Biblioteket - A til Å",
-          href: "/",
-        },
-        {
-          title: "Internetadgang",
-          href: "/",
-        },
-        {
-          title: "Print, scan og kopi",
-          href: "/",
-        },
-        {
-          title: "Booking af lokaler",
-          href: "/",
-        },
-        {
-          title: "Nyhedsbrev",
-          href: "/",
-        },
-      ],
-    })
-  );
-
   const dropdownList = [
     {
       title: "DK",
@@ -75,7 +37,7 @@ export const Footer = () => {
               <a href="/">Se bibliotekernes åbningstider</a>
             </div>
           </div>
-          <Accordion list={accordionList} />
+          <Accordion list={list} />
           <div className="footer-widgets mt-48">
             <Dropdown list={dropdownList} ariaLabel="dropdown" />
             <Logo fallback={false} libraryName={""} altText={"logo"} />
@@ -108,10 +70,30 @@ export const Footer = () => {
             </div>
 
             <div className="footer-site-info--icons mt-32">
-              <a href="/"><img src={`icons/social/icon-social-facebook.svg`} alt="Facebook link" /></a>
-              <a href="/"><img src={`icons/social/icon-social-instagram.svg`} alt="Instagram link" /></a>
-              <a href="/"><img src={`icons/social/icon-social-youtube.svg`} alt="Youtube link" /></a>
-              <a href="/"><img src={`icons/social/icon-social-spotify.svg`} alt="Spotify link" /></a>
+              <a href="/">
+                <img
+                  src={`icons/social/icon-social-facebook.svg`}
+                  alt="Facebook link"
+                />
+              </a>
+              <a href="/">
+                <img
+                  src={`icons/social/icon-social-instagram.svg`}
+                  alt="Instagram link"
+                />
+              </a>
+              <a href="/">
+                <img
+                  src={`icons/social/icon-social-youtube.svg`}
+                  alt="Youtube link"
+                />
+              </a>
+              <a href="/">
+                <img
+                  src={`icons/social/icon-social-spotify.svg`}
+                  alt="Spotify link"
+                />
+              </a>
             </div>
           </div>
         </Pagefold>
@@ -133,52 +115,28 @@ export const Footer = () => {
                 </div>
               </div>
             </div>
-            <div className="footer-column">
-              <h4 className="text-header-h4">Om bibliotekerne</h4>
-              <ul>
-                <li className="footer-column--link">
-                  <a href="/">Brug af bibliotekerne</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Erstatninger og gebyrer</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Opret bruger</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Biblioteket - A til Å</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Internetadgang</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Print, scan og kopi</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Booking af lokaler</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Nyhedsbrev</a>
-                </li>
-              </ul>
-            </div>
-            <div className="footer-column">
-              <h4 className="text-header-h4">Online tilbud</h4>
-              <ul>
-                <li className="footer-column--link">
-                  <a href="/">Litteratursiden</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Bibliotek.dk</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">eReolen</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Filmstriben</a>
-                </li>
-              </ul>
-            </div>
+            <FooterColumn
+              title={"Om bibliotekerne"}
+              links={[
+                "Brug af bibliotekerne",
+                "Erstatninger og gebyrer",
+                "Opret bruger",
+                "Biblioteket - A til Å",
+                "Internetadgang",
+                "Print, scan og kopi",
+                "Booking af lokaler",
+                "Nyhedsbrev",
+              ]}
+            />
+            <FooterColumn
+              title={"Online tilbud"}
+              links={[
+                "Litteratursiden",
+                "Bibliotek.dk",
+                "eReolen",
+                "Filmstriben",
+              ]}
+            />
             <div className="footer-column">
               <h4 className="text-header-h4 mb-16">Kontakt</h4>
               <p className="text-body-medium-regular">
@@ -193,57 +151,33 @@ export const Footer = () => {
                 <a href="/">info@ltk.dk</a>
               </div>
             </div>
-            <div className="footer-column">
-              <h4 className="text-header-h4">Text</h4>
-              <ul>
-                <li className="footer-column--link">
-                  <a href="/">Lorem ipsum a la lorem ip</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Bibliotekerne lorem </a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Labore ipsum</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Adipisci velit le lorem</a>
-                </li>
-              </ul>
-            </div>
-            <div className="footer-column">
-              <h4 className="text-header-h4">Text</h4>
-              <ul>
-                <li className="footer-column--link">
-                  <a href="/">Lorem ipsum a la lorem ip</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Bibliotekerne lorem </a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Labore ipsum</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Adipisci velit le lorem</a>
-                </li>
-              </ul>
-            </div>
-            <div className="footer-column">
-              <h4 className="text-header-h4">Text</h4>
-              <ul>
-                <li className="footer-column--link">
-                  <a href="/">Lorem ipsum a la lorem ip</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Bibliotekerne lorem </a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Labore ipsum</a>
-                </li>
-                <li className="footer-column--link">
-                  <a href="/">Adipisci velit le lorem</a>
-                </li>
-              </ul>
-            </div>
+            <FooterColumn
+              title={"Text"}
+              links={[
+                "Lorem ipsum a la lorem ip",
+                "Bibliotekerne lorem",
+                "Labore ipsum",
+                "Adipisci velit le lorem",
+              ]}
+            />
+            <FooterColumn
+              title={"Text"}
+              links={[
+                "Lorem ipsum a la lorem ip",
+                "Bibliotekerne lorem",
+                "Labore ipsum",
+                "Adipisci velit le lorem",
+              ]}
+            />
+            <FooterColumn
+              title={"Text"}
+              links={[
+                "Lorem ipsum a la lorem ip",
+                "Bibliotekerne lorem",
+                "Labore ipsum",
+                "Adipisci velit le lorem",
+              ]}
+            />
           </div>
 
           <div className="footer-widgets mt-80">
@@ -255,10 +189,30 @@ export const Footer = () => {
 
           <div className="footer-site-info">
             <div className="footer-site-info--icons">
-              <a href="/"><img src={`icons/social/icon-social-facebook.svg`} alt="Facebook link" /></a>
-              <a href="/"><img src={`icons/social/icon-social-instagram.svg`} alt="Instagram link" /></a>
-              <a href="/"><img src={`icons/social/icon-social-youtube.svg`} alt="Youtube link" /></a>
-              <a href="/"><img src={`icons/social/icon-social-spotify.svg`} alt="Spotify link" /></a>
+              <a href="/">
+                <img
+                  src={`icons/social/icon-social-facebook.svg`}
+                  alt="Facebook link"
+                />
+              </a>
+              <a href="/">
+                <img
+                  src={`icons/social/icon-social-instagram.svg`}
+                  alt="Instagram link"
+                />
+              </a>
+              <a href="/">
+                <img
+                  src={`icons/social/icon-social-youtube.svg`}
+                  alt="Youtube link"
+                />
+              </a>
+              <a href="/">
+                <img
+                  src={`icons/social/icon-social-spotify.svg`}
+                  alt="Spotify link"
+                />
+              </a>
             </div>
             <div className="footer-site-info--links">
               <a href="/" className="footer-site-info--link">
