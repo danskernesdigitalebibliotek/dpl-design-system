1d5ec7e48f0436edb7c1166f4c0679b8582a9316
matis-dk <mathiasp50@gmail.com>
2021-12-07 10:00:49 +0100

    Added number story

---
+++ src/stories/number/Number.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+
+export type NumberProps = {
+  label: number;
+  status: "danger" | "warning" | "info" | "neutral";
+};
+
+export const Number = (props: NumberProps) => {
+  const { label, status } = props;
+
+  function getStatus() {
+    if (status === "danger") return "number-danger";
+    if (status === "warning") return "number-warning";
+    if (status === "info") return "number-info";
+    if (status === "neutral") return "number-neutral";
+    return "";
+  }
+
+  return (
+    <div className={`number  ${getStatus()}`}>
+      <span>{label}</span>
+    </div>
+  );
+};
+
+declare global {
+  interface Window {
+    eventHeader: () => void;
+  }
+}
