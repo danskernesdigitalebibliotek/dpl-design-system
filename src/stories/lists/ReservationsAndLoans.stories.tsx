52a8d9c24875b9388bf2313abc85bda2be912f3b
matis-dk <mathiasp50@gmail.com>
2021-12-09 13:17:05 +0100

    Adding List Reservation

--- src/stories/lists/ReservationsAndLoans.stories.tsx
+++ src/stories/lists/ReservationsAndLoans.stories.tsx
@@ -1,20 +1,197 @@
 import React from "react";
-import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { withDesign } from "storybook-addon-designs";
-import { MissingStory } from "../missing-story/MissingStory";
-import { addMissingLabel } from "../missing-story/tools";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ReservationsAndLoans } from "./ReservationsAndLoans";
 
 export default {
-  title: addMissingLabel("Components / Lists"),
-  component: MissingStory,
+  title: "Components / Lists",
+  component: ReservationsAndLoans,
   decorators: [withDesign],
-  argTypes: {
-    name: {
-      defaultValue: '"Reservations and Loans" list',
-    }
-  },
+  argTypes: {},
   parameters: {},
-} as ComponentMeta<typeof MissingStory>;
+} as ComponentMeta<typeof ReservationsAndLoans>;
 
-export const ReservationsAndLoansList: ComponentStory<typeof MissingStory> = MissingStory.bind({});
-ReservationsAndLoansList.storyName = addMissingLabel("Reservations and Loans");
+const Template: ComponentStory<typeof ReservationsAndLoans> = (args) => (
+  <ReservationsAndLoans {...args} />
+);
+
+export const Item1 = Template.bind({});
+Item1.args = {
+  material: {
+    type: "bog",
+    title: "Mennesket i naturen",
+    description: "Af Anton M. Berg (2020)",
+    image: "images/book_cover_5.jpg",
+    url: "",
+    noteLabel: "Du pålægges et gebyr, når materialet afleveres",
+    noteUrl: "",
+    noteAsWarning: true,
+  },
+  isStacked: false,
+  fee: 0,
+  counter: {
+    isReady: false,
+    label: "dage",
+    percentage: 100,
+    status: "danger",
+    value: 0,
+  },
+  statusLabel: {
+    label: "overskrevet",
+    status: "danger",
+  },
+  statusNotes: "Afleveres 02.10.2021",
+};
+
+export const Item2 = Template.bind({});
+Item2.args = {
+  material: {
+    type: "bog",
+    title: "Audrey Hepburn",
+    description: "Af Isabel Sánchez Vegara, Amaia Arrazola (2018)",
+    image: "images/book_cover_3.jpg",
+    url: "",
+    noteLabel: "Du pålægges et gebyr, når materialet afleveres",
+    noteUrl: "",
+    noteAsWarning: false,
+  },
+  isStacked: false,
+  fee: 0,
+  counter: {
+    isReady: false,
+    label: "dage",
+    percentage: 100,
+    status: "danger",
+    value: 0,
+  },
+  statusLabel: {
+    label: "overskrevet",
+    status: "danger",
+  },
+  statusNotes: "Afleveres 02.10.2021",
+};
+
+export const Item3 = Template.bind({});
+Item3.args = {
+  material: {
+    type: "bog",
+    title: "Sauna",
+    description: "Af Anton M. Berg (2020)",
+    image: "images/book_cover_6.jpg",
+    url: "",
+    noteLabel: "",
+    noteUrl: "",
+    noteAsWarning: false,
+  },
+  isStacked: false,
+  fee: 0,
+  counter: {
+    isReady: false,
+    label: "dage",
+    percentage: 75,
+    status: "warning",
+    value: 7,
+  },
+  statusLabel: {
+    label: "udløber snart",
+    status: "warning",
+  },
+  statusNotes: "Afleveres 02.10.2021",
+};
+
+export const Item4 = Template.bind({});
+Item4.args = {
+  material: {
+    type: "bog",
+    title: "De Uadskillige",
+    description: "Af Anton M. Berg (2020)",
+    image: "images/book_cover_1.jpg",
+    url: "",
+    noteLabel: "",
+    noteUrl: "",
+    noteAsWarning: false,
+  },
+  isStacked: false,
+  fee: 0,
+  counter: {
+    isReady: false,
+    label: "dage",
+    percentage: 60,
+    status: "warning",
+    value: 9,
+  },
+  statusLabel: undefined,
+  statusNotes: "Afleveres 02.10.2021",
+};
+
+export const Item5 = Template.bind({});
+Item5.args = {
+  material: {
+    type: "bog",
+    title: "Den lille bog om dansk design - for børn og barnlige sjæle",
+    description: "Af Marie Hugsted (2018)",
+    image: "images/book_cover_2.jpg",
+    url: "",
+    noteLabel: "",
+    noteUrl: "",
+    noteAsWarning: false,
+  },
+  isStacked: false,
+  fee: 0,
+  counter: {
+    isReady: false,
+    label: "dage",
+    percentage: 50,
+    status: "neutral",
+    value: 24,
+  },
+  statusLabel: undefined,
+  statusNotes: "Afleveres 02.10.2021",
+};
+
+export const Item6 = Template.bind({});
+Item6.args = {
+  material: {
+    type: "bog",
+    title: "Døgnkioskmennesket",
+    description: "Af Sayaka Murata (2019)",
+    image: "images/book_cover_2.jpg",
+    url: "",
+    noteLabel: "",
+    noteUrl: "",
+    noteAsWarning: false,
+  },
+  isStacked: false,
+  fee: 0,
+  counter: {
+    isReady: false,
+    label: "dage",
+    percentage: 25,
+    status: "neutral",
+    value: 29,
+  },
+  statusLabel: undefined,
+  statusNotes: "Afleveres 02.10.2021",
+};
+
+export const ItemStacked = Template.bind({});
+ItemStacked.args = {
+  material: {
+    type: "bog",
+    title: "Audrey Hepburn",
+    description: "Af Isabel Sánchez Vegara, Amaia Arrazola (2018)",
+    image: "images/book_cover_3.jpg",
+    url: "",
+    noteLabel: "+ 3 materialer",
+    noteUrl: "",
+    noteAsWarning: false,
+  },
+  isStacked: true,
+  fee: 20,
+  counter: undefined,
+  statusLabel: {
+    label: "afleveret for sent",
+    status: "danger",
+  },
+  statusNotes: "Afleveres 10.10.2020",
+};
---
+++ src/stories/lists/ReservationsAndLoans.tsx
@@ -0,0 +1,122 @@
+import React, { useEffect } from "react";
+import { Counter, CounterProps } from "../counter/Counter";
+import { StatusLabel, StatusLabelProps } from "../status-label/StatusLabel";
+import { ArrowUI } from "../arrows/icon-arrow-ui/ArrowUI.stories";
+
+export type ReservationsAndLoansProps = {
+  material: {
+    type: string;
+    title: string;
+    description: string;
+    image: string;
+    url: string;
+    noteLabel: string;
+    noteUrl: string;
+    noteAsWarning: boolean;
+  };
+  isStacked: boolean;
+  counter: CounterProps;
+  statusLabel: StatusLabelProps;
+  statusNotes: string;
+  fee: number;
+};
+
+export const ReservationsAndLoans = (props: ReservationsAndLoansProps) => {
+  const { material, isStacked, counter, statusLabel, statusNotes, fee } = props;
+
+  return (
+    <div className="list-container">
+      <div className="list-reservation">
+        <div className="list-reservation__material">
+          <div>
+            <ImageMaterial url={material.image} />
+            <a
+              href={material.noteUrl}
+              style={{ height: "14px", fontSize: "12px" }}
+              className={`list-reservation__note-mobile text-small-caption ${
+                material.noteAsWarning
+                  ? "color-signal-alert"
+                  : "color-secondary-gray"
+              }`}
+            >
+              {material.noteLabel}
+            </a>
+          </div>
+          <div className="list-reservation__information">
+            <div>
+              <span className="list-reservation__type">{material.type}</span>
+            </div>
+            <div className="list-reservation__about">
+              <p className="text-header-h4">{material.title}</p>
+              <p
+                className="text-small-caption color-secondary-gray"
+                style={{ marginTop: "2px" }}
+              >
+                {material.description}
+              </p>
+            </div>
+            <a
+              href={material.noteUrl}
+              style={{ height: "14px", fontSize: "12px" }}
+              className={`list-reservation__note-desktop text-small-caption ${
+                material.noteAsWarning
+                  ? "color-signal-alert"
+                  : "color-secondary-gray"
+              }`}
+            >
+              {material.noteLabel}
+            </a>
+          </div>
+        </div>
+
+        <div className="list-reservation__status">
+          <div className="list-reservation__counter">
+            {counter && (
+              <Counter
+                label={counter.label}
+                percentage={counter.percentage}
+                value={counter.value}
+                status={counter.status}
+                isReady={counter.isReady}
+              />
+            )}
+          </div>
+          <div>
+            <div className="list-reservation__deadline">
+              {statusLabel && (
+                <StatusLabel
+                  label={statusLabel.label}
+                  status={statusLabel.status}
+                />
+              )}
+              <p className="text-small-caption">{statusNotes}</p>
+            </div>
+          </div>
+          <div className="list-reservation__fee">
+            {fee ? (
+              <p className="text-body-medium-medium">Gebyr {fee},-</p>
+            ) : (
+              <ArrowUI direction="right" hover={false} size="small" />
+            )}
+          </div>
+        </div>
+      </div>
+      {isStacked && <div className="list-reservation__stacked_1" />}
+      {isStacked && <div className="list-reservation__stacked_2" />}
+    </div>
+  );
+};
+
+function ImageMaterial({ url }: { url: string }) {
+  return (
+    <div className="image-material">
+      <img src={"url"} alt="" />
+    </div>
+  );
+}
+
+declare global {
+  interface Window {
+    eventHeader: () => void;
+  }
+}
