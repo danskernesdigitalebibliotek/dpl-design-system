cc61d06112c03b1d12c8c653566bbe3561cf2388
Adam Antal <adam@reload.dk>
2022-03-29 17:12:13 +0200

    Created Availability Label component - created folder structure for the component with a .tsx, stories.tsx, and .scss file - updated typography to be used in the Availability label - added comments to base.scss and AL import

---
+++ src/stories/availability-label/AvailabilityLabel.stories.tsx
@@ -0,0 +1,56 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
+import { AvailabilityLabel } from "./AvailabilityLabel";
+
+export default {
+  title: "Components / Availability Label",
+  component: AvailabilityLabel,
+  decorators: [withDesign],
+  argTypes: {},
+  parameters: {},
+} as ComponentMeta<typeof AvailabilityLabel>;
+
+const Template: ComponentStory<typeof AvailabilityLabel> = (args) => (
+  <AvailabilityLabel {...args} />
+);
+
+export const Available = Template.bind({});
+Available.args = {
+  manifestation: "bog",
+  availability: "Hjemme",
+  status: "available",
+  pagefoldProps: {
+    inherit: false,
+    container: false,
+    size: "xsmall",
+    classes: "availability-label--unselected text-label",
+    colorClass: "success",
+  },
+};
+
+export const Selected = Template.bind({});
+Selected.args = {
+  manifestation: "ebog",
+  availability: "Online",
+  status: "selected",
+  pagefoldProps: {
+    inherit: false,
+    container: false,
+    size: "none",
+    classes: "availability-label--selected text-label",
+  },
+};
+
+export const Unavailable = Template.bind({});
+Unavailable.args = {
+  manifestation: "lydbog (cd-mp3)",
+  availability: "Udlånt",
+  status: "unavailable",
+  pagefoldProps: {
+    inherit: false,
+    container: false,
+    size: "xsmall",
+    classes: "availability-label--unselected text-label",
+    colorClass: "alert",
+  },
+};
---
+++ src/stories/availability-label/AvailabilityLabel.tsx
@@ -0,0 +1,47 @@
+import { Pagefold, PagefoldProps } from "../pagefold/Pagefold";
+
+export type AvailabilityLabelPropsType = {
+  manifestation: "bog" | "ebog" | "lydbog (net)" | "lydbog (cd-mp3)";
+  availability: "Hjemme" | "Online" | "Udlånt";
+  status: "available" | "unavailable" | "selected";
+  pagefoldProps: PagefoldProps;
+};
+
+export const AvailabilityLabel = (props: AvailabilityLabelPropsType) => {
+  const { manifestation, availability, status } = props;
+
+  const pagefoldProps = { ...props.pagefoldProps };
+
+  function getPageFoldProps(
+    currentStatus: AvailabilityLabelPropsType["status"]
+  ) {
+    if (currentStatus === "selected") {
+      pagefoldProps.size = "none";
+      pagefoldProps.classes = "availability-label--selected text-label";
+    }
+    if (currentStatus === "available") {
+      pagefoldProps.size = "xsmall";
+      pagefoldProps.classes = "availability-label--unselected text-label";
+      pagefoldProps.colorClass = "success";
+    }
+    if (currentStatus === "unavailable") {
+      pagefoldProps.size = "xsmall";
+      pagefoldProps.classes = "availability-label--unselected text-label";
+      pagefoldProps.colorClass = "alert";
+    }
+    return pagefoldProps;
+  }
+
+  return (
+    <Pagefold {...getPageFoldProps(status)}>
+      <img
+        className={`availability-label--check ${status}`}
+        src="icons/collection/Check.svg"
+        alt="check-icon"
+      />
+      <p className="text-label-semibold ml-24">{manifestation.toUpperCase()}</p>
+      <div className="availability-label--divider ml-4" />
+      <p className="text-label-normal ml-4 mr-8">{availability}</p>
+    </Pagefold>
+  );
+};
