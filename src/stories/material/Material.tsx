3bbc0680795e3da1911e4db9030f82652252369d
Adam Antal <adam@reload.dk>
2022-04-28 16:17:41 +0200

    Define tint classes in a separate constant for "Material" component
    Increases code readability in the material component.

--- src/stories/material/Material.tsx
+++ src/stories/material/Material.tsx
@@ -8,18 +8,20 @@ export type MaterialProps = {
 };
 
 export const Material = (props: MaterialProps) => {
-  const { size, animate, url } = props;
+  const { size, animate, url, tint } = props;
+
+  const tintClasses = {
+    "120": "bg-identity-tint-120",
+    "80": "bg-identity-tint-80",
+    "60": "bg-identity-tint-60",
+    "40": "bg-identity-tint-40",
+    "20": "bg-identity-tint-20",
+  };
 
   const classes = {
-    wrapper: clsx(
-      `material material--${size}`,
-      { "bg-identity-tint-120": props.tint === "120" },
-      { "bg-identity-tint-80": props.tint === "80" },
-      { "bg-identity-tint-60": props.tint === "60" },
-      { "bg-identity-tint-40": props.tint === "40" },
-      { "bg-identity-tint-20": props.tint === "20" },
-      { material__animate: animate }
-    ),
+    wrapper: clsx(`material material--${size}`, tintClasses[tint], {
+      material__animate: animate,
+    }),
   };
 
   return (
