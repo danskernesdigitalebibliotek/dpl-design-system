0558571fee1eaa77a13cdbe1eab88a31433371c2
Kasper Birch <kasperbirch1@gmail.com>
2022-05-27 13:15:26 +0200

    refactor component IconHeart / IconFavourite
    refactor component name + and resolve all comments from previous pull request.

--- src/stories/icon/icon-heart/IconFavourite.tsx
+++ src/stories/icon/icon-heart/IconFavourite.tsx
@@ -1,8 +1,10 @@
-export type IconHeartProps = {
+export type IconFavouriteProps = {
   fill?: boolean;
 };
 
-export const IconHeart = ({ fill }: IconHeartProps) => {
+export const IconFavourite = ({ fill }: IconFavouriteProps) => {
+  // This svg is a copy from public/icons/basic/icon-heart.svg.
+  // It is made as inline svg to be able to change fill value from props
   return (
     <svg
       width="24"
@@ -14,7 +16,7 @@ export const IconHeart = ({ fill }: IconHeartProps) => {
       <path
         d="M11.5 20L10.2675 18.921C5.89 15.1035 3 12.5858 3 9.49591C3 6.9782 5.057 5 7.675 5C9.154 5 10.5735 5.66213 11.5 6.70845C12.4265 5.66213 13.846 5 15.325 5C17.943 5 20 6.9782 20 9.49591C20 12.5858 17.11 15.1035 12.7325 18.9292L11.5 20Z"
         stroke="black"
-        stroke-width="2"
+        strokeWidth="2"
       />
     </svg>
   );
