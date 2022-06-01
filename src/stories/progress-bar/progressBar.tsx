59b4c719f404df6edd11f028def911a8121fb15e
Sine Jespersen <sinejespersen@gmail.com>
2022-06-01 13:22:38 +0200

    CMN-213: added progress bar atom

---
+++ src/stories/progress-bar/progressBar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export type ProgressBarProps = {
+  amount: number;
+  fullAmount: number;
+  title: string;
+  outOf: string
+};
+
+export const ProgressBar = (props: ProgressBarProps) => {
+  const { amount, fullAmount, title, outOf } = props;
+  const width = (amount/fullAmount)*100;
+  
+  return (
+    <div className="dpl-progress-bar text-small-caption color-secondary-gray">
+      <div className="dpl-progress-bar__header mb-8">
+        <div>{title}</div>
+        <div>{amount} {outOf} {fullAmount}</div>
+      </div>
+      <div className="dpl-progress-bar__progress-bar bg-global-secondary">
+        <div className="bg-identity-primary" style={{ width: `${width}%` }}></div>
+      </div>
+  </div>
+  );
+};
