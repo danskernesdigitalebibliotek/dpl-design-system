eed1057d4111a5d226267e6d13f5d4fa8b0526d8
Adam Antal <adam@reload.dk>
2022-06-01 14:24:30 +0200

    Comply with BEM css structuring for the autosuggest-text component
    The container class before looked like a modifier (--container), and
    needed to be fixed. BEM structure - http://getbem.com/introduction/

--- src/stories/autosuggest-text/AutosuggestText.tsx
+++ src/stories/autosuggest-text/AutosuggestText.tsx
@@ -5,10 +5,10 @@ export type AutosuggestTextProps = {
 export const AutosuggestText = (props: AutosuggestTextProps) => {
   const { items } = props;
   return (
-    <ul className="suggest-text--container">
+    <ul className="autosuggest-text-container">
       {items.map((item) => {
         return (
-          <li className="suggest-text--item text-body-medium-regular">
+          <li className="autosuggest-text-container__item text-body-medium-regular">
             {item}
           </li>
         )
