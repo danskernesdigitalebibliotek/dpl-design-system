2237d14c135646ad457743d2d73b56c36d756db9
kasperbirch1 <kasperbirch1@gmail.com>
2022-06-01 13:06:53 +0200

    Move favourite svg size control to css insted of inlinline values

--- src/stories/icon/icon-favourite/IconFavourite.tsx
+++ src/stories/icon/icon-favourite/IconFavourite.tsx
@@ -8,8 +8,6 @@ export const IconFavourite = ({ fill }: IconFavouriteProps) => {
   return (
     <svg
       className="icon-favourite"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill={fill ? "#000" : "none"}
       xmlns="http://www.w3.org/2000/svg"
