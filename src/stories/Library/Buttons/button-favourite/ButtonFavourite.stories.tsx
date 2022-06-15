63f56ed60ab9b0eaf421b4839cb1f10e3e5d53fc
Jesper Pedersen <pejes@aarhus.dk>
2022-06-15 12:11:43 +0200

    CMN-213: Re-structure into Library and Blocks

---
+++ src/stories/Library/Buttons/button-favourite/ButtonFavourite.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
+import { ButtonFavourite } from "./ButtonFavourite";
+
+export default {
+  title: "Library / Buttons / Button Favourite",
+  component: ButtonFavourite,
+  decorators: [withDesign],
+  parameters: {
+    design: {
+      type: "figma",
+      url:
+        "https://www.figma.com/file/Zx9GrkFA3l4ISvyZD2q0Qi/Designsystem?node-id=656%3A5407",
+    },
+  },
+  argTypes: {
+    fill: {
+      control: "boolean",
+      defaultValue: true,
+    },
+  },
+} as ComponentMeta<typeof ButtonFavourite>;
+
+const Template: ComponentStory<typeof ButtonFavourite> = (args) => (
+  <ButtonFavourite {...args} />
+);
+
+export const favourite = Template.bind({});
+favourite.args = {};
---
+++ src/stories/Library/search-result-item/SearchResultItem.tsx
@@ -0,0 +1,81 @@
+import { AvailabilityLabel } from "../availability-label/AvailabilityLabel";
+import { Material } from "../material/Material";
+import { ReactComponent as ArrowSmallRight } from "../Arrows/icon-arrow-ui/icon-arrow-ui-small-right.svg";
+import { ButtonFavourite } from "../Buttons/button-favourite/ButtonFavourite";
+
+export type SearchResultItemProps = {
+  heartFill?: boolean;
+  title: string;
+  author: string;
+  year: string;
+  seriesNumber?: string;
+  series?: string;
+};
+
+export const SearchResultItem = ({
+  heartFill,
+  title,
+  author,
+  year,
+  seriesNumber,
+  series,
+}: SearchResultItemProps) => {
+  return (
+    <a href="/" className="search-result-item arrow arrow__hover--right-small">
+      <div className="search-result-item__cover">
+        <Material
+          url="images/book_cover_3.jpg"
+          size="small"
+          animate={false}
+          tint="120"
+        />
+      </div>
+      <div className="search-result-item__text">
+        <div className="search-result-item__meta">
+          <ButtonFavourite fill={heartFill} />
+
+          {seriesNumber && (
+            <div className="text-small-caption">
+              <span className="text-label-semibold">{`Nr. ${seriesNumber} `}</span>
+              {series && (
+                <>
+                  <span>i serien </span>
+                  <a href="/" className="link-tag">
+                    {series}
+                  </a>
+                </>
+              )}
+            </div>
+          )}
+        </div>
+        <h2 className="search-result-item__title text-header-h4">{title}</h2>
+        <p className="text-small-caption">{`Af ${author} (${year})`}</p>
+      </div>
+      <div className="search-result-item__availability">
+        <AvailabilityLabel
+          manifestation="Bog"
+          availability="Hjemme"
+          status="available"
+        />
+        <AvailabilityLabel
+          manifestation="Ebog"
+          availability="Online"
+          status="available"
+        />
+
+        <AvailabilityLabel
+          manifestation="Lydbog (cd-mp3)"
+          availability="Udlånt"
+          status="unavailable"
+        />
+        <AvailabilityLabel
+          manifestation="Lydbog (net)"
+          availability="Online"
+          status="available"
+        />
+      </div>
+
+      <ArrowSmallRight />
+    </a>
+  );
+};
