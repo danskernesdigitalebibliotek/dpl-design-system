b73f53d78246fdf48373999844be00cf9a19b2d0
Adam Antal <adam@reload.dk>
2022-06-15 13:08:21 +0200

    Add Autosuggestmaterial component, its story and styling to the project
    This commmit introduces the Autosuggest subcomponent -
    autosuggest-material. It shows up to three items as suggestions in the
    autosuggest dropdown with their cover art, author, work title and its
    release date.

---
+++ src/stories/autosuggest-material/AutosuggestMaterial.stories.tsx
@@ -0,0 +1,79 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
+import { AutosuggestMaterial, AutosuggestMaterialProps } from "./AutosuggestMaterial";
+
+export default {
+  title: "Components / Autosuggest - Material",
+  component: AutosuggestMaterial,
+  decorators: [withDesign],
+  argTypes: {
+    items: {
+      name: "Items",
+      defaultValue: [
+        {
+          cover: "https://imgcdn.saxo.com/_9780345514400/0x500",
+          title: "I know why the caged bird sings",
+          author: "Maya Angelou",
+          year: "1969",
+        },{
+          cover: "https://imgcdn.saxo.com/_9781616550417/0x500",
+          title: "The Legend of Zelda: Hyrule Historia",
+          author: "Patrick Thorpe, Michael Gombos, Takahiro Moriki, et al.",
+          year: "2013",
+        },{
+          cover: "https://imgcdn.saxo.com/_9781449401160/0x500",
+          title: "5 very good reasons to punch a dolphin in the mouth and other useful guides",
+          author: "Matthew Inman",
+          year: "2010",
+        }
+      ],
+      control: { type: "array" }
+    }
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url:
+        "https://www.figma.com/file/ETOZIfmgGS1HUfio57SOh7/S%C3%B8gning?node-id=4709%3A24976",
+    },
+  },
+} as ComponentMeta<typeof AutosuggestMaterial>;
+
+const Template: ComponentStory<typeof AutosuggestMaterial> = (args: AutosuggestMaterialProps) => (
+  // Apart from AutosuggestMaterial, everything else is here just for the story
+  // context. AutosuggestMaterial style is directly dependent on the header,
+  // search field, and AutosuggestText styling.
+  // If you find out this context no longer represents reality, please adjust it.
+  <>
+    <header className="header" style={{height: "144px"}}>
+      <div className="header__logo-desktop">
+        <p className="text-body-medium-regular">Context</p>
+      </div>
+      <div className="header__menu">
+        <nav className="header__menu-first">
+          <p className="text-body-medium-regular">Context</p>
+        </nav>
+        <div className="header__menu-second">
+          <div className="header__menu-search">
+            <input
+                  className="header__menu-search-input text-body-medium-regular"
+                  type="text"
+                  placeholder="This field is here just for context."
+                />
+            <img
+                  className="header__menu-search-icon"
+                  src={`icons/basic/icon-search.svg`}
+                  alt="search icon"
+                />
+            <AutosuggestMaterial {...args} />
+          </div>
+        </div>
+      </div>
+      <div className="header__clock">
+        <p className="text-body-medium-regular">Context</p>
+      </div>
+    </header>
+  </>
+);
+
+export const MaterialSuggestion = Template.bind({});
---
+++ src/stories/autosuggest-material/AutosuggestMaterial.tsx
@@ -0,0 +1,40 @@
+import { Material } from "../material/Material";
+
+export type AutosuggestMaterialProps = {
+  items: {
+    cover: string;
+    title: string;
+    author: string;
+    year: string;
+  }[]
+}
+
+export const AutosuggestMaterial = (props: AutosuggestMaterialProps) => {
+  const {items} = props
+
+  return (
+    <ul className="autosuggest">
+      <li className="autosuggest__item--text text-body-medium-regular">
+            I am a contextual text item
+      </li>
+      <hr className="autosuggest__divider"/>
+      <ul className="autosuggest__materials">
+        {items.map((item) => {
+          return (
+            <li className="autosuggest__materials__item">
+              <div className="autosuggest__materials__item__content">
+                <div className="autosuggest__materials__item__content__cover">
+                  <Material size="xsmall" animate={true} url={item.cover} />
+                </div>
+                <div className="autosuggest__materials__item__content__info">
+                  <div className="text-body-medium-medium autosuggest__materials__item__content__info__title">{item.title}</div>
+                  <div className="text-body-small-regular autosuggest__materials__item__content__info__author">{`${item.author} (${item.year})`}</div>
+                </div>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </ul>
+  )
+}
