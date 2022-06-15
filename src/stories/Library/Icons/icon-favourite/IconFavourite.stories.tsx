83e148f92672b4e832793f02d1c013d45e9adb77
Jesper Pedersen <pejes@aarhus.dk>
2022-06-15 12:11:43 +0200

    CMN-213: Re-structure into Library and Blocks

---
+++ src/stories/Library/Icons/icon-favourite/IconFavourite.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
+import { IconFavourite } from "./IconFavourite";
+
+export default {
+  title: "Library / Icons",
+  component: IconFavourite,
+  decorators: [withDesign],
+  parameters: {
+    design: {
+      type: "figma",
+      url:
+        "https://www.figma.com/file/Zx9GrkFA3l4ISvyZD2q0Qi/Designsystem?node-id=656%3A5407",
+    },
+  },
+  argTypes: {
+    fill: {
+      control: "boolean",
+      defaultValue: true,
+    },
+  },
+} as ComponentMeta<typeof IconFavourite>;
+
+const Template: ComponentStory<typeof IconFavourite> = (args) => (
+  <IconFavourite {...args} />
+);
+
+export const favourite = Template.bind({});
+favourite.args = {};
---
+++ src/stories/Library/typography/Typography.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { withDesign } from "storybook-addon-designs";
+import { Meta } from "@storybook/react";
+
+import { Typography } from "./Typography";
+
+export default {
+  title: "Library / Typography",
+  component: Typography,
+  decorators: [withDesign],
+  parameters: {
+    design: {
+      type: "figma",
+      url:
+        "https://www.figma.com/file/Zx9GrkFA3l4ISvyZD2q0Qi/Designsystem?node-id=2%3A476",
+    },
+  },
+} as Meta;
+
+export const Default = () => <Typography />;
