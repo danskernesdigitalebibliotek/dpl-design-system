d535c661ff5b9ba13c7ff075e259597a312b5c90
Adam Antal <adam@reload.dk>
2022-06-16 11:49:11 +0200

    Add value to textContent story argument for Modal text story
    Our github action was failing because of this.

--- src/stories/modal-text/ModalText.stories.tsx
+++ src/stories/modal-text/ModalText.stories.tsx
@@ -26,5 +26,14 @@ Text.args = {
   subtitle: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   linkText: "Ikke nu",
   btnLabel: "Giv samtykke",
-  textContent,
+  textContent: [
+    {
+      title: "item 1 title",
+      text: "item 1 text"
+    },
+    {
+      title: "item 2 title",
+      text: "item 2 text"
+    }
+  ],
 };
