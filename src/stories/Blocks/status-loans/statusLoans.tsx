fdcc3d47d93a41b49e11b58bf84431a3e2a33fac
Jesper Pedersen <pejes@aarhus.dk>
2022-06-16 16:14:10 +0200

    CMN-213: Update structure of loan/reservation status and its sub componenets

--- src/stories/Blocks/status-loans/statusLoans.tsx
+++ src/stories/Blocks/status-loans/statusLoans.tsx
@@ -1,27 +1,24 @@
 import React from "react";
-import {
-  StatusBarElement,
-  StatusBarElementProps,
-} from "../status-bars-element/statusBarElement";
+import { Status, StatusProps } from "../../Library/status/status";
 
 export type LinkProps = {
   link: string;
   text: string;
 };
 
-export type StatusBarComponentProps = {
-  statusBarsData: StatusBarElementProps[];
+export type StatusLoansProps = {
+  statusBarsData: StatusProps[];
   title: string;
   link: LinkProps;
   bread: string;
 };
 
-export const StatusBarComponent = (props: StatusBarComponentProps) => {
+export const StatusLoans = (props: StatusLoansProps) => {
   const { statusBarsData, title: statusBarsTitle, link, bread } = props;
   const { link: url, text: linkText } = link;
   console.log(statusBarsData);
   return (
-    <div className="dpl-status-bar-component">
+    <div className="dpl-status-loans">
       <h1 className="text-header-h1 m-8">{statusBarsTitle}</h1>
       <div className="m-8 text-body-small-regular">
         {`${bread} `}
@@ -29,12 +26,9 @@ export const StatusBarComponent = (props: StatusBarComponentProps) => {
           {linkText}
         </a>
       </div>
-      <div className="dpl-status-bar-component__status-bar-containers">
+      <div className="dpl-status-loans__column">
         {statusBarsData.map(({ statusBars, title }) => (
-          <StatusBarElement
-            title={title}
-            statusBars={statusBars}
-          ></StatusBarElement>
+          <Status title={title} statusBars={statusBars}></Status>
         ))}
       </div>
     </div>
