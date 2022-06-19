bc13848c92486f82c171c90ad6a97f59a0ab70f0
Adam Antal <adam@reload.dk>
2022-06-19 22:05:24 +0200

    Only use li inside ul in autosuggest-text as per Eslint rules
    I was using an unordered list inside of an unordered list. This had to
    be changed because unordered lists shouldn't contain other ul's
    directly.

--- src/stories/autosuggest-text/AutosuggestText.tsx
+++ src/stories/autosuggest-text/AutosuggestText.tsx
@@ -6,15 +6,17 @@ export const AutosuggestText = (props: AutosuggestTextProps) => {
   const { items } = props;
   return (
     <ul className="autosuggest pb-16">
-      <ul>
-        {items.map((item) => {
-          return (
-            <li className="autosuggest__text text-body-medium-regular px-24">
-              {item}
-            </li>
-          )
-        })}
-      </ul>
+      <li>
+        <ul>
+          {items.map((item) => {
+            return (
+              <li className="autosuggest__text text-body-medium-regular px-24">
+                {item}
+              </li>
+            )
+          })}
+        </ul>
+      </li>
     </ul>
   )
 }
