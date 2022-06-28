b68421c80538cedb1e35e9f00b1e53ac579251d3
kasperbirch1 <kasperbirch1@gmail.com>
2022-06-28 08:51:25 +0200

    Change material cover render
    Adds the same logic as used in dpl-react to determine if it is a tag or a span image to display in

--- src/stories/material/Material.stories.tsx
+++ src/stories/material/Material.stories.tsx
@@ -6,7 +6,40 @@ export default {
   title: "Atoms / Material",
   component: Material,
   decorators: [withDesign],
-  argTypes: {},
+  argTypes: {
+    size: {
+      name: "Image size",
+      control: "radio",
+    },
+    tint: {
+      name: "Background color tint",
+      control: "radio",
+    },
+    animate: {
+      name: "Use animation",
+      control: "boolean",
+    },
+    materialUrl: {
+      name: "Material URL",
+      control: "text",
+    },
+    materialDescription: {
+      name: "Material description",
+      control: "text",
+    },
+    url: {
+      name: "Cover URL",
+      control: "text",
+    },
+  },
+  args: {
+    size: "small",
+    animate: false,
+    url: "images/book_cover_3.jpg",
+    tint: "120",
+    materialUrl: "https://www.google.com",
+    materialDescription: "/",
+  },
   parameters: {},
 } as ComponentMeta<typeof Material>;
 
@@ -15,25 +48,27 @@ const Template: ComponentStory<typeof Material> = (args) => (
 );
 
 export const MaterialVisible = Template.bind({});
-MaterialVisible.args = {
-  url: "images/book_cover_3.jpg",
-  size: "small",
-  animate: false,
-  tint: "120",
-};
+MaterialVisible.args = {};
 
 export const MaterialNotVisible = Template.bind({});
 MaterialNotVisible.args = {
   url: "",
-  size: "small",
-  animate: false,
   tint: "60",
+  materialUrl: "",
 };
 
 export const MaterialAnimated = Template.bind({});
 MaterialAnimated.args = {
-  url: "images/book_cover_3.jpg",
+  animate: true,
+  tint: "80",
+  materialUrl: "",
+};
+
+export const MaterialLinked = Template.bind({});
+MaterialLinked.args = {
   animate: true,
   size: "small",
   tint: "80",
+  materialUrl: "/",
+  materialDescription: "Cover of Audrey Hepburn",
 };
