d0803569735f87420ed0d2a08d732447fe19da2e
kasperbirch1 <kasperbirch1@gmail.com>
2022-07-11 09:35:11 +0200

    Move import React from "react" to start

--- src/stories/Library/material-page/MaterialPage.stories.tsx
+++ src/stories/Library/material-page/MaterialPage.stories.tsx
@@ -1,7 +1,7 @@
+import React from "react";
 import { withDesign } from "storybook-addon-designs";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import MaterialPage from "./MaterialPage";
-import React from "react";
 
 export default {
   title: "Blocks / Material Page",
