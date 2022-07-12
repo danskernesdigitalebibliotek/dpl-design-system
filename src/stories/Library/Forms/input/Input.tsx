c699ef62fe735a6e8efa6b3da60d63b6c2e9af11
Sine Jespersen <sinejespersen@gmail.com>
2022-07-12 14:13:53 +0200

    5398: add input with validation

---
+++ src/stories/Library/Forms/input/Input.tsx
@@ -0,0 +1,36 @@
+export type InputProps = {
+  label: string;
+  type: "text" | "password";
+  id: string;
+  description: string;
+  validation: string;
+};
+
+const Input = (props: InputProps) => {
+  const { label, type, id, description, validation } = props;
+  const invalid = validation ? "true" : "false";
+  return (
+    <div className="dpl-input">
+      <label htmlFor={id}>{label}</label>
+      <input
+        aria-invalid={invalid}
+        aria-describedby={`description-${id}`}
+        aria-labelledby={validation ? `validation-${id}` : ""}
+        id={id}
+        type={type}
+      />
+      {description && (
+        <div className="dpl-input__description" id={`description-${id}`}>
+          {description}
+        </div>
+      )}
+      {validation && (
+        <div id={`validation-${id}`} className="dpl-input__validation">
+          {validation}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Input;
