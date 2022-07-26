9b121e32f0213d70a7d81bcf34f573e488b00807
Jesper Pedersen <pejes@aarhus.dk>
2022-07-26 07:02:41 +0200

    5398: Add avatar for modal header

---
+++ src/stories/Library/avatar/Avatar.tsx
@@ -0,0 +1,9 @@
+export const Avatar = () => {
+  return (
+    <div className="avatar bg-global-secondary">
+      <img src="icons/collection/Profile.svg" alt="Profile avatar" />
+    </div>
+  );
+};
+
+export default Avatar;
