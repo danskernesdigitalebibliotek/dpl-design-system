2e1a1030ec8e23f1d297b27ce26e7753ac6a927b
Adam Antal <adoantal@gmail.com>
2022-07-28 12:06:58 +0200

    Fix console errors in list description component
    React wasn't happy about us not providing each map rendered child
    component a unique id. Now we do :)

--- src/stories/Library/Lists/list-description/ListDescription.tsx
+++ src/stories/Library/Lists/list-description/ListDescription.tsx
@@ -1,3 +1,5 @@
+import { generateId } from "../../horizontal-term-line/HorizontalTermLine";
+
 export type ListData = {
   [k: string]: { value: string; type: "standard" | "link" };
 };
@@ -8,16 +10,16 @@ const ListDescription: React.FC<{ data: ListData; className?: string }> = ({
 }) => {
   return (
     <dl className={`list-description ${className ?? ""}`}>
-      {Object.keys(data).map((key) => {
+      {Object.keys(data).map((key, index) => {
         const { value, type } = data[key as keyof ListData];
         return (
-          <>
+          <div key={generateId(index)}>
             <dt>{key}:</dt>
             <dd>
               {type === "standard" && value}
               {type === "link" && <span className="link-tag">{value}</span>}
             </dd>
-          </>
+          </div>
         );
       })}
     </dl>
