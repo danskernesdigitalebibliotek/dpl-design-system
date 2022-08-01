de760d169a8daf55471ecada443ce70099f20385
Adam Antal <adoantal@gmail.com>
2022-08-01 12:23:30 +0200

    Add the Review component, its story and styling
    The design brief can be found at:
    https://www.figma.com/file/ETOZIfmgGS1HUfio57SOh7/S%C3%B8gning?node-id=1030%3A16233

---
+++ src/stories/Library/review/Review.stories.tsx
@@ -0,0 +1,51 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
+import { Reviews as ReviewsComp } from "./Review";
+
+export default {
+  title: "Library / Reviews",
+  component: ReviewsComp,
+  decorators: [withDesign],
+  argTypes: {
+    numberOfReviews: {
+      defaultValue: 1,
+    },
+    metaHeadline: {
+      defaultValue: "Berlingske tidende, 2021.03.19",
+      control: "text",
+    },
+    hearts: {
+      defaultValue: 5,
+    },
+    headlineText: {
+      defaultValue: "Anmeldelse af: Efterskælv",
+      control: "text",
+    },
+    bodyText: {
+      defaultValue:
+        "Den anerkendte forfatter tager udgangspunkt i sit ægteskabs nøgne efterskælv. Veloplagt, men rystet, byder hun indenfor - også i de mørkeste kamre. Der har aldrig før været en skilsmisse i denne klan....",
+      control: "text",
+    },
+    linkText: {
+      defaultValue: "Berlingske tidende, 2021.03.19",
+      control: "text",
+    },
+    linkLink: {
+      defaultValue: "/",
+      control: "text",
+    },
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/ETOZIfmgGS1HUfio57SOh7/S%C3%B8gning?node-id=1030%3A16233",
+    },
+  },
+} as ComponentMeta<typeof ReviewsComp>;
+
+const Template: ComponentStory<typeof ReviewsComp> = (args) => {
+  return <ReviewsComp {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {};
---
+++ src/stories/Library/review/Review.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+
+export type ReviewsProps = {
+  numberOfReviews: number;
+  metaHeadline: string;
+  hearts: 1 | 2 | 3 | 4 | 5 | 6;
+  headlineText: string;
+  bodyText?: string;
+  linkText?: string;
+  linkLink: string;
+};
+
+export const Reviews: React.FC<ReviewsProps> = ({
+  numberOfReviews,
+  metaHeadline,
+  hearts,
+  headlineText,
+  bodyText,
+  linkText,
+  linkLink,
+}) => {
+  const filledHeartsArray = Array.from({ length: hearts }, (_, i) => i + 1);
+  const emptyHeartsArray = Array.from({ length: 6 - hearts }, (_, i) => i + 1);
+  const reviewsArray = Array.from({ length: numberOfReviews }, (_, i) => i + 1);
+  return (
+    <ul className="reviews">
+      {reviewsArray.map((reviewNumber) => {
+        return (
+          <li className="review text-small-caption" key={reviewNumber}>
+            <div className="review__meta mb-8">{metaHeadline}</div>
+            <div className="mb-4">
+              {filledHeartsArray.map((value) => {
+                return (
+                  <img
+                    key={value}
+                    src="icons/basic/icon-heart-filled.svg"
+                    alt="icon-heart-filled"
+                    className="review__heart"
+                  />
+                );
+              })}
+              {emptyHeartsArray.map((value) => {
+                return (
+                  <img
+                    key={value}
+                    src="icons/basic/icon-heart-grey.svg"
+                    alt="icon-heart-grey"
+                    className="review__heart"
+                  />
+                );
+              })}
+            </div>
+            <div className="review__headline mb-8">{headlineText}</div>
+            {bodyText && <p className="review__body mb-8">{bodyText}</p>}
+            {linkText && (
+              <a href={linkLink}>
+                <div className="link-tag text-small-caption mb-8">
+                  {linkText}
+                </div>
+              </a>
+            )}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Reviews;
