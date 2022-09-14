6574a3593d6ab643210ecdaf6d9266fe2e68de25
Adam Antal <adoantal@gmail.com>
2022-09-14 15:19:35 +0200

    Target BEM elements in find on shelf list row, not HTML elements

--- src/stories/Library/Lists/list-find-on-shelf/ListFindOnShelf.tsx
+++ src/stories/Library/Lists/list-find-on-shelf/ListFindOnShelf.tsx
@@ -16,18 +16,18 @@ const ListFindOnShelf: React.FC<ListFindOnShelfProps> = ({
   return (
     <ul className="find-on-shelf">
       <li className="find-on-shelf__header-row text-small-caption">
-        <span className="find-on-shelf__material-title">Materiale</span>
+        <span className="find-on-shelf__material-header">Materiale</span>
         <span>Find det på hylden</span>
-        <span className="find-on-shelf__item-count-title">Hjemme</span>
+        <span className="find-on-shelf__item-count-header">Hjemme</span>
       </li>
       {numberArray.map((key) => {
         return (
           <li className="find-on-shelf__row text-body-medium-regular" key={key}>
-            <span className="find-on-shelf__material-title">
+            <span className="find-on-shelf__material-text">
               {manifestationName}
             </span>
-            <span>{location}</span>
-            <span>
+            <span className="find-on-shelf__location-text">{location}</span>
+            <span className="find-on-shelf__item-count-text">
               {nrAvailable}
               <span className="hide-on-desktop"> hjemme</span>
             </span>
