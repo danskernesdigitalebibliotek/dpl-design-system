a52d24f2847e793ff046635e5bf1903d580f38b2
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-09-14 11:49:05 +0200

    Use more representative list of libraries
    Makes most sense to show libraries within the same municipality

--- src/stories/Blocks/reservation/forms/Collect.tsx
+++ src/stories/Blocks/reservation/forms/Collect.tsx
@@ -1,26 +1,38 @@
-import { FC } from "react";
 import { Dropdown } from "../../../Library/dropdown/Dropdown";
 import Form from "./Form";
 
-// export interface UserDataProps {}
-
-// const UserData: FC<UserDataProps> = ({}) => {
-const Collect: FC = () => {
-  return (
-    <Form
-      title="Afhentningsbibliotek"
-      description={["Hvor ønsker du at afhente dine bøger?"]}
-    >
-      <Dropdown
-        ariaLabel="Afheningsbibliotek"
-        list={[
-          { title: "Hovedbiblioteket i Birkerød", href: "" },
-          { title: "Københavns hovedbibliotek", href: "" },
-        ]}
-        arrowIcon="chevron"
-      />
-    </Form>
-  );
-};
+const Collect = () => (
+  <Form
+    title="Afhentningsbibliotek"
+    description={["Hvor ønsker du at afhente dine bøger?"]}
+  >
+    <Dropdown
+      ariaLabel="Afheningsbibliotek"
+      list={[
+        { title: "Hovedbiblioteket, Krystalgade", href: "" },
+        { title: "Blågårdens Bibliotek", href: "" },
+        { title: "Brønshøj Bibliotek", href: "" },
+        { title: "Christianshavns Bibliotek", href: "" },
+        { title: "Husum Bibliotek", href: "" },
+        { title: "Islands Brygge Bibliotek", href: "" },
+        { title: "Øbro Jagtvej Bibliotek", href: "" },
+        { title: "Biblioteket Rentemestervej", href: "" },
+        { title: "Nørrebro Bibliotek", href: "" },
+        { title: "Solvang Bibliotek", href: "" },
+        { title: "Sundby Bibliotek", href: "" },
+        { title: "Bibliotekshuset", href: "" },
+        { title: "Sydhavnens Bibliotek", href: "" },
+        { title: "Tingbjerg Bibliotek", href: "" },
+        { title: "Valby Bibliotek", href: "" },
+        { title: "Kulturstationen Vanløse. Biblioteket", href: "" },
+        { title: "Vesterbro Bibliotek", href: "" },
+        { title: "Vigerslev Bibliotek", href: "" },
+        { title: "Østerbro Bibliotek", href: "" },
+        { title: "Ørestad Bibliotek", href: "" },
+      ]}
+      arrowIcon="chevron"
+    />
+  </Form>
+);
 
 export default Collect;
