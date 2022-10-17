a159cf3daa048d66dc1957d57bdb7797998ed971
kasperbirch1 <kasperbirch1@gmail.com>
2022-10-17 16:52:25 +0200

    Add FacetBrowser + .modal--no-padding + make disclosure icon optional
    The following has been added
    - FacetBrowser + FacetBrowser story 
    a component that contains everything that needs to be used to display a facet browser modal
    - FacetBrowserTag
    has an onClick that toggles the ccs class facet-browser-tag--selected
    - Facet browser dummy data
    a list of facet groups and their tags retrieved from facet-browser-dummy-data.ts
    
    Other changes
    - Make disclosure icon optional
    This is because we should not have an icon in the facet browser disclosure
    - Add  ccs class .modal--no-padding 
    Since disclosures in the facet browser must go out to the edge, we need to be able to neutralize padding on the modal

---
+++ src/stories/Library/Modals/modal-facet-browser/facet-browser-dummy-data.ts
@@ -0,0 +1,23 @@
+const facetBrowserDummyData = [
+  {
+    title: "Literær Form",
+    tags: [
+      "Skønlitteratur (96)",
+      "Faglitteratur (302)",
+      "Film (28)",
+      "Romaner (46)",
+      "Digte (2)",
+      "Noveller (2)",
+      "Tegneserier (1)",
+    ],
+  },
+  { title: "Audio", tags: ["Lydbøger (1)", "Lydfiler (1)"] },
+  { title: "Film", tags: ["Film1", "Film2", "Film3"] },
+  { title: "Spil", tags: ["Spil1", "Spil2", "Spil3"] },
+  { title: "Forfatter", tags: ["Forfatter1", "Forfatter2", "Forfatter3"] },
+  { title: "Årstal", tags: ["1991", "1992"] },
+  { title: "Genre", tags: ["Genre1", "Genre2", "Genre3"] },
+  { title: "Emne", tags: ["Emne1", "Emne2", "Emne3"] },
+];
+
+export default facetBrowserDummyData;
