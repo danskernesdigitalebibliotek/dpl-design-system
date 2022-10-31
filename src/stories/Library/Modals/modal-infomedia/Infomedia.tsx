9a2c81ddfe35cbff656761f11ff1b3bc54f7e499
kasperbirch1 <kasperbirch1@gmail.com>
2022-10-31 11:41:10 +0100

    Add Infomedia modal + Logo
    This modal will be used to show infomedia articles.

---
+++ src/stories/Library/Modals/modal-infomedia/Infomedia.tsx
@@ -0,0 +1,27 @@
+import Modal from "../Modal";
+
+export type InfomediaProps = {
+  showModal: boolean;
+  title: string;
+  text: string;
+};
+
+export const Infomedia = (props: InfomediaProps) => {
+  const { title, text, showModal } = props;
+  return (
+    <Modal shownModal={showModal} classNames="">
+      <article>
+        <img
+          className="infomedia-logo"
+          src="icons/logo/infomedia-logo.svg"
+          alt=""
+        />
+        <h2>{title}</h2>
+        <div
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: text }}
+        />
+      </article>
+    </Modal>
+  );
+};
