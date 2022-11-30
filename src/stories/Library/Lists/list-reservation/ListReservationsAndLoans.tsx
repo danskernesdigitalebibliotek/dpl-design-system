19e8a520372e3de53ff962aa5b06882cafde02b6
Sine Jespersen <sinejespersen@gmail.com>
2022-11-30 21:49:09 +0100

    5389: list reservation note added

--- src/stories/Library/Lists/list-reservation/ListReservationsAndLoans.tsx
+++ src/stories/Library/Lists/list-reservation/ListReservationsAndLoans.tsx
@@ -49,7 +49,7 @@ export const ListReservationsAndLoans = (
             <button
               type="button"
               aria-label="note about material"
-              className={`list-reservation__note-desktop ${
+              className={`list-reservation__note list-reservation__note--desktop ${
                 cover.noteAsWarning
                   ? "color-signal-alert"
                   : "color-secondary-gray"
@@ -83,7 +83,7 @@ export const ListReservationsAndLoans = (
               <p className="text-small-caption">{statusNotes}</p>
               <a
                 href={cover.noteUrl}
-                className={`list-reservation__note-mobile ${
+                className={`list-reservation__note list-reservation__note--mobile ${
                   cover.noteAsWarning
                     ? "color-signal-alert"
                     : "color-secondary-gray"
