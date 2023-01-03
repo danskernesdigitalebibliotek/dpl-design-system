24da030351363631724e73c8f83c6167bede8a2f
Sine Jespersen <sinejespersen@gmail.com>
2023-01-03 20:02:21 +0100

    5464: fixes from commens on pr

--- src/stories/Library/recommender/Recommender.stories.tsx
+++ src/stories/Library/recommender/Recommender.stories.tsx
@@ -1,12 +1,41 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-
 import Recommender from "./Recommender";
 
 export default {
   title: "Library / Recommender",
   component: Recommender,
-  argTypes: {},
-  parameters: {},
+  argTypes: {
+    title: {
+      control: "text",
+      defaultValue:
+        "Kunne du lide “Audrey Hepburn” og mangler du noget at læse?",
+    },
+    recommenderData: {
+      control: "object",
+      defaultValue: [
+        {
+          title: "Ella Fitzgerald",
+          authors: "Af Isabel Sánchez Vegara",
+          filled: true,
+        },
+        {
+          title: "Ella Fitzgerald",
+          authors: "Af Isabel Sánchez Vegara",
+          filled: true,
+        },
+        {
+          title: "Ella Fitzgerald",
+          authors: "Af Isabel Sánchez Vegara",
+          filled: false,
+        },
+        {
+          title: "Ella Fitzgerald",
+          authors: "Af Isabel Sánchez Vegara",
+          filled: false,
+        },
+      ],
+    },
+  },
 } as ComponentMeta<typeof Recommender>;
 
 const Template: ComponentStory<typeof Recommender> = (args) => (
--- src/stories/Library/recommender/Recommender.tsx
+++ src/stories/Library/recommender/Recommender.tsx
@@ -1,41 +1,52 @@
 import { Cover } from "../cover/Cover";
 import { ReactComponent as SvgIcon } from "../Icons/icon-favourite/icon-favourite.svg";
 
-const Recommender: React.FC = () => {
+export type RecommenderData = {
+  title: string;
+  authors: string;
+  filled: boolean;
+};
+export type RecommenderProps = {
+  recommenderData: RecommenderData[];
+  title: string;
+};
+
+const Recommender: React.FC<RecommenderProps> = ({
+  recommenderData,
+  title,
+}) => {
   return (
     <div className="recommender">
-      <h1 className="text-header-h1">
-        Kunne du lide “Audrey Hepburn” og mangler du noget at læse?
-      </h1>
-      <div className="recommender-grid">
-        {Array.from(Array(4).keys()).map(() => (
-          <div className="recommender__grid__material">
-            <div className="button-favourite">
-              <SvgIcon
-                className={
-                  Math.random() < 0.5
-                    ? "icon-favourite icon-favourite--filled"
-                    : "icon-favourite"
-                }
+      <h2 className="recommender__title text-header-h1">{title}</h2>
+      <ul className="recommender__grid">
+        {recommenderData.map(({ title: recTitle, filled, authors }) => (
+          <li className="recommender-material">
+            <div className="recommender-material__favourite">
+              <div className="button-favourite button-favourite">
+                <SvgIcon
+                  className={`icon-favourite icon-favourite--bright ${
+                    filled ? "icon-favourite--bright-filled" : ""
+                  }`}
+                />
+              </div>
+            </div>
+            <div className="recommender-material__cover-container ">
+              <Cover
+                url="images/book_cover_3.jpg"
+                size="medium"
+                animate={false}
+                tint="120"
               />
             </div>
-            <Cover
-              url="images/book_cover_3.jpg"
-              size="medium"
-              animate={false}
-              tint="120"
-            />
-            <div className="recommender__grid__material__text">
-              <div className="recommender__grid__material__text__title">
-                En bog
-              </div>
-              <div className="recommender__grid__material__text__author">
-                Af forfatter1 og forfatter2
+            <div className="recommender-material__meta">
+              <h3 className="recommender-material__meta__title">{recTitle}</h3>
+              <div className="recommender-material__meta__author">
+                {authors}
               </div>
             </div>
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
