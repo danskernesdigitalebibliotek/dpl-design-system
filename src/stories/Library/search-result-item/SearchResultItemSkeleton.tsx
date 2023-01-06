ad3a2740eff765716ba0a44fba44ee0cd508b044
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-01-06 20:06:26 +0100

    Add Skeleton Screen CSS for loading state style
    and implement it on a search result

---
+++ src/stories/Library/search-result-item/SearchResultItemSkeleton.tsx
@@ -0,0 +1,14 @@
+export const SearchResultItemSkeleton = () => {
+  return (
+    <div className="search-result-item arrow arrow__hover--right-small ssc">
+      <div className="ssc-square">&nbsp;</div>
+      <div className="ssc-wrapper w-80">
+        <div className="ssc-head-line w-60 mb" />
+        <div className="ssc-line w-60 mbs">&nbsp;</div>
+        <div className="ssc-line w-60 mbs">&nbsp;</div>
+      </div>
+    </div>
+  );
+};
+
+export default {};
