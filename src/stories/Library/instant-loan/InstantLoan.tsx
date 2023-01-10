32e11ade72c9082a3467f8328974cb38725fa47b
Kasper Birch <kasperbirch1@gmail.com>
2023-01-10 15:04:39 +0100

    Use DisclosureControllable instead of <details> + Split InstantLoan
    Replaces the usage of the <details> and <summary> HTML elements with a custom DisclosureControllable component. This provides more control over the state of the disclosure component and allows for the open/closed state to be controlled programmatically.
    
    - Split InstantLoan component into 3 separate components: InstantLoanSummary, InstantLoanBranches, and DisclosureControllable for better separation of code and a more modular structure.

--- src/stories/Library/instant-loan/InstantLoan.tsx
+++ src/stories/Library/instant-loan/InstantLoan.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import AvailabilityLabel from "../availability-label/AvailabilityLabel";
-import { Cover } from "../cover/Cover";
+import DisclosureControllable from "../disclosure-controllable/DisclosureControllable";
+import InstantLoanBranches from "./InstantLoanBranches";
+import InstantLoanSummary from "./InstantLoanSummary";
 
 type InstantLoanProps = {
   title: string;
@@ -17,48 +18,24 @@ const InstantLoan: React.FunctionComponent<InstantLoanProps> = ({
   InstantLoanBranchButton,
   instantLoanBranches,
 }) => (
-  <details className="instant-loan pagefold-parent--small">
-    <summary className="instant-loan-summary cursor-pointer p-24">
-      <div className="pagefold-triangle--small" />
-      <Cover
-        url="images/book_cover_3.jpg"
-        size="small"
-        animate={false}
-        tint="120"
+  <DisclosureControllable
+    showContent
+    id="instant-loan"
+    detailsClassName="instant-loan pagefold-parent--small"
+    summaryClassName="instant-loan-summary cursor-pointer p-24"
+    summary={
+      <InstantLoanSummary
+        title={title}
+        subTitle={subTitle}
+        underlineDescription={underlineDescription}
       />
-      <div>
-        <h2 className="text-header-h4 mt-22 mb-4">{title}</h2>
-        <p className="text-small-caption">{subTitle}</p>
-        <div className="instant-loan-underline mt-35">
-          <p className="link-tag text-small-caption">{underlineDescription}</p>
-          <img
-            className="instant-loan-arrow"
-            src="icons/collection/ExpandMore.svg"
-            alt="various-icon"
-          />
-        </div>
-      </div>
-    </summary>
-
-    <ul className="instant-loan-branches">
-      {instantLoanBranches.map((branch) => (
-        <li>
-          <div className="instant-loan-branch cursor-pointer px-24">
-            <p className="text-header-h5">{branch}</p>
-            <AvailabilityLabel
-              manifestationType="Bog"
-              availability="Hjemme"
-              status="available"
-              quantity={8}
-            />
-            <button className="link-tag text-small-caption instant-loan-branch__show-on-map">
-              {InstantLoanBranchButton}
-            </button>
-          </div>
-        </li>
-      ))}
-    </ul>
-  </details>
+    }
+  >
+    <InstantLoanBranches
+      branches={instantLoanBranches}
+      InstantLoanBranchButton={InstantLoanBranchButton}
+    />
+  </DisclosureControllable>
 );
 
 export default InstantLoan;
