ecc3891b743356ab0437a616e66d97b2553f2770
Kasper Birch <kasperbirch1@gmail.com>
2023-01-24 16:02:49 +0100

    Refactoring the Cover component (Part1)
    There are major changes in the Cover Component. Now it renders a CoverImage as a div with a img inside. If there is not a src for the img or if the src is returning an error the div wrapper will have css classes for aspect ratio and background color tint.   
    
    This is because we would like to be able to show cover images in the original aspect ratio without a background if the cover image is smaller than the parent div

--- src/stories/Library/cover/Cover.tsx
+++ src/stories/Library/cover/Cover.tsx
@@ -1,50 +1,31 @@
-import clsx from "clsx";
+import CoverImage from "./CoverImage";
+import { CoverProps } from "./types";
 
-export type CoverProps = {
-  url: string;
-  animate: boolean;
-  size: "xsmall" | "small" | "medium" | "large" | "xlarge";
-  tint?: "20" | "40" | "80" | "100" | "120";
-  coverUrl?: string;
-  coverDescription?: string;
+const Cover = ({
+  size,
+  animate,
+  src,
+  tint,
+  coverUrl,
+  description,
+}: CoverProps) => {
+  if (coverUrl && description) {
+    // Images inside links must have an non-empty alt text to meet accessibility requirements.
+    // Only render the cover as a link if we have both an url and a description.
+    return (
+      <a href={coverUrl}>
+        <CoverImage
+          src={src}
+          description={description}
+          size={size}
+          animate={animate}
+          tint={tint}
+        />
+      </a>
+    );
+  }
+
+  return <CoverImage src={src} size={size} animate={animate} tint={tint} />;
 };
 
-export const Cover = (props: CoverProps) => {
-  const { size, animate, url, tint, coverUrl, coverDescription } = props;
-
-  type TintClassesType = {
-    [key: string]: string;
-  };
-  const tintClasses: TintClassesType = {
-    default: "bg-identity-tint-120",
-    "120": "bg-identity-tint-120",
-    "100": "bg-identity-tint-100",
-    "80": "bg-identity-tint-80",
-    "40": "bg-identity-tint-40",
-    "20": "bg-identity-tint-20",
-  };
-
-  const classes = {
-    wrapper: clsx(`cover cover--${size}`, tintClasses[tint || "default"], {
-      cover__animate: animate,
-    }),
-  };
-
-  const materialCover = url && <img src={url} alt={coverDescription || ""} />;
-
-  return (
-    <div className="cover-container">
-      {/**
-       * Images inside links must have an non-empty alt text to meet accessibility requirements.
-       * Only render the cover as a link if we have both an url and a description.
-       */}
-      {coverUrl && coverDescription ? (
-        <a href={coverUrl} className={classes.wrapper}>
-          {materialCover}
-        </a>
-      ) : (
-        <span className={classes.wrapper}>{materialCover}</span>
-      )}
-    </div>
-  );
-};
+export default Cover;
---
+++ src/stories/Library/cover/helper.ts
@@ -0,0 +1,15 @@
+type TintClassesType = {
+  [key: string]: string;
+};
+
+export const tintClasses: TintClassesType = {
+  default: "bg-identity-tint-120",
+  "120": "bg-identity-tint-120",
+  "100": "bg-identity-tint-100",
+  "80": "bg-identity-tint-80",
+  "40": "bg-identity-tint-40",
+  "20": "bg-identity-tint-20",
+};
+
+
+export default {};
