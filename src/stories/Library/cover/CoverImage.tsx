c874781996f687b656bc27d407dcd763b8f8e253
Kasper Birch <kasperbirch1@gmail.com>
2023-01-24 16:02:49 +0100

    Refactoring the Cover component (Part1)
    There are major changes in the Cover Component. Now it renders a CoverImage as a div with a img inside. If there is not a src for the img or if the src is returning an error the div wrapper will have css classes for aspect ratio and background color tint.   
    
    This is because we would like to be able to show cover images in the original aspect ratio without a background if the cover image is smaller than the parent div

---
+++ src/stories/Library/cover/CoverImage.tsx
@@ -0,0 +1,36 @@
+import clsx from "clsx";
+import { FC, useState } from "react";
+import { tintClasses } from "./helper";
+import { CoverProps } from "./types";
+
+const CoverImage: FC<CoverProps> = ({
+  src,
+  size,
+  description,
+  tint,
+  animate,
+}) => {
+  const [imageError, setImageError] = useState<boolean | null>(null);
+  return (
+    <div
+      className={clsx(
+        (!src || imageError) && `cover-size--${size}`,
+        (!src || imageError) && `cover-aspect--${size}`,
+        (!src || imageError) && tintClasses[tint || "default"]
+      )}
+    >
+      {src && (
+        <img
+          onError={() => setImageError(true)}
+          className={clsx("cover-img", `cover-size--${size}`, {
+            "cover-animate": animate,
+          })}
+          src={src}
+          alt={description || ""}
+        />
+      )}
+    </div>
+  );
+};
+
+export default CoverImage;
---
+++ src/stories/Library/cover/types.ts
@@ -0,0 +1,8 @@
+export type CoverProps = {
+  src: string;
+  animate: boolean;
+  size: "xsmall" | "small" | "medium" | "large" | "xlarge";
+  tint: "20" | "40" | "80" | "100" | "120";
+  coverUrl?: string;
+  description?: string;
+};
