25f32a0f40ef9c56ad7313ee2d82427f67d588f6
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-01-28 10:45:08 +0100

    Add better length for search result skeleton lines
    And vary depending on screen size so it follows the real layout more or
    less

--- src/stories/Library/search-result-item/SearchResultItemSkeleton.tsx
+++ src/stories/Library/search-result-item/SearchResultItemSkeleton.tsx
@@ -2,10 +2,10 @@ export const SearchResultItemSkeleton = () => {
   return (
     <div className="search-result-item arrow arrow__hover--right-small ssc">
       <div className="ssc-square">&nbsp;</div>
-      <div className="ssc-wrapper w-80">
-        <div className="ssc-head-line w-60 mb" />
-        <div className="ssc-line w-60 mbs">&nbsp;</div>
-        <div className="ssc-line w-60 mbs">&nbsp;</div>
+      <div className="ssc-wrapper">
+        <div className="ssc-head-line mb" />
+        <div className="ssc-line mbs">&nbsp;</div>
+        <div className="ssc-line mbs">&nbsp;</div>
       </div>
     </div>
   );
