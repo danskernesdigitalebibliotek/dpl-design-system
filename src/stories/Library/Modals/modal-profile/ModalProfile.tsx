174d6e7256602fbbf4659697e658928082edd57d
Adam Antal <adoantal@gmail.com>
2023-02-06 15:54:39 +0100

    Remove backdrop from profile modal so it's not doubled

--- src/stories/Library/Modals/modal-profile/ModalProfile.tsx
+++ src/stories/Library/Modals/modal-profile/ModalProfile.tsx
@@ -24,45 +24,43 @@ export const ModalProfile: React.FC<ModalProfileProps> = ({
   notifications,
   profileNavLinks,
 }) => (
-  <div className="modal-backdrop">
-    <Modal shownModal={showModal} classNames="modal-profile modal-right">
-      <ModalHeader
-        headerName={headerName}
-        headerLinkHref={headerLinkHref}
-        headerLinkText={headerLinkText}
-      />
+  <Modal shownModal={showModal} classNames="modal-profile modal-right">
+    <ModalHeader
+      headerName={headerName}
+      headerLinkHref={headerLinkHref}
+      headerLinkText={headerLinkText}
+    />
 
-      <div className="modal-profile__notifications">
-        {notifications.map((item, index) => (
-          <div key={index} className="modal-profile__notification-item">
-            <ListDashboard
-              title={item.title}
-              number={item.number}
-              label={item.label}
-              showDot={item.showDot}
-              href={item.href}
-            />
-          </div>
-        ))}
-      </div>
-
-      <div className="modal-profile__container">
-        <div className="modal-profile__links">
-          <LinkFilters filters={profileNavLinks} />
-        </div>
-        <div className="modal-profile__btn-logout">
-          <Button
-            buttonType="default"
-            size="medium"
-            variant="filled"
-            label="Log ud"
-            disabled={false}
-            collapsible={false}
+    <div className="modal-profile__notifications">
+      {notifications.map((item, index) => (
+        <div key={index} className="modal-profile__notification-item">
+          <ListDashboard
+            title={item.title}
+            number={item.number}
+            label={item.label}
+            showDot={item.showDot}
+            href={item.href}
           />
         </div>
+      ))}
+    </div>
+
+    <div className="modal-profile__container">
+      <div className="modal-profile__links">
+        <LinkFilters filters={profileNavLinks} />
       </div>
-    </Modal>
-  </div>
+      <div className="modal-profile__btn-logout">
+        <Button
+          buttonType="default"
+          size="medium"
+          variant="filled"
+          label="Log ud"
+          disabled={false}
+          collapsible={false}
+        />
+      </div>
+    </div>
+  </Modal>
 );
 
 export default ModalProfile;
