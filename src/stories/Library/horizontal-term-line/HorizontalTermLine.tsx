b566713f76e5d74226130f3b2b8b960753cb51c1
Kasper Birch <kasperbirch1@gmail.com>
2023-02-09 13:50:52 +0100

    Add show more feature to HorizontalTermLine component
    'HorizontalTermLine' has been updated to display only a limited number of links. A show more button has been added, which, when clicked, displays all the links.

--- src/stories/Library/horizontal-term-line/HorizontalTermLine.tsx
+++ src/stories/Library/horizontal-term-line/HorizontalTermLine.tsx
@@ -1,3 +1,6 @@
+import clsx from "clsx";
+import { useState } from "react";
+
 export interface HorizontalTermLineList {
   url: string;
   text: string;
@@ -19,19 +22,43 @@ const HorizontalTermLine: React.FC<HorizontalTermLineProps> = ({
   subTitle,
   linkList,
 }) => {
+  const numberOfItemsToShow = 2;
+  const [showMore, setShowMore] = useState(false);
+  const itemsToShow = showMore
+    ? linkList
+    : linkList.slice(0, numberOfItemsToShow);
+  const showMoreButton = linkList.length > numberOfItemsToShow;
+
   return (
     <div className="text-small-caption horizontal-term-line">
       <p className="text-label-bold">
         {`${title}`}{" "}
         {subTitle && <span className="text-small-caption">{subTitle} </span>}
       </p>
-      {linkList.map((link, index) => (
+
+      {itemsToShow.map((link, index) => (
         <span key={generateId(index)}>
           <a href="/" className="link-tag" key={index}>
             {link.text}
           </a>
         </span>
       ))}
+
+      {showMoreButton && (
+        <button
+          type="button"
+          onClick={() => setShowMore(!showMore)}
+          aria-label="Expand More"
+        >
+          <img
+            className={clsx("horizontal-term-line__expand", {
+              "horizontal-term-line__expand--expanded": showMore,
+            })}
+            src="icons/collection/ExpandMore.svg"
+            alt=""
+          />
+        </button>
+      )}
     </div>
   );
 };
