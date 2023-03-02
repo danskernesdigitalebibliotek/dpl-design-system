dad2b308f21173b1e7a35b5595d5833d0d5d0600
Adam Antal <adoantal@gmail.com>
2023-03-02 14:48:52 +0100

    Add mobile viewport size for testing with Chromatic

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,7 +6,11 @@ export const parameters = {
       date: /Date$/,
     },
   },
-  // Notifies Chromatic to pause the animations when they finish at a global level
-  // This is because Chromatic doesn't show the cover image when the animation starts with opacity 0
-  chromatic: { pauseAnimationAtEnd: true },
+  chromatic: {
+    // Notifies Chromatic to pause the animations when they finish at a global level
+    // This is because Chromatic doesn't show the cover image when the animation starts with opacity 0
+    pauseAnimationAtEnd: true,
+    // Set the viewports that Chromatic tests with globally.
+    viewports: [400, 1200]
+  },
 };
