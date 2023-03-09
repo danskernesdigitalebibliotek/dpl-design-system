b0431bf228ab0826d68c9234cfb2088a6059f78d
Adam Antal <adoantal@gmail.com>
2023-03-09 11:29:13 +0100

    Explain why 400 & 1200 values were chosen for Chromatic viewports

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -11,6 +11,8 @@ export const parameters = {
     // This is because Chromatic doesn't show the cover image when the animation starts with opacity 0
     pauseAnimationAtEnd: true,
     // Set the viewport widths that Chromatic tests with globally.
+    // 400 lies between our screen-xs & screen-s variables.
+    // 1200 is the chromatic viewport we were using as default before adding an extra one for phones.
     viewports: [400, 1200]
   },
 };
