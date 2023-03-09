9ce711bdaf6e11204ab3b54d4f4da1b09422ae55
Adam Antal <adoantal@gmail.com>
2023-03-09 11:19:59 +0100

    Use "viewport widths" instead of "viewports" in preview.js
    Co-authored-by: Kasper Garnæs <kasperg@users.noreply.github.com>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -10,7 +10,7 @@ export const parameters = {
     // Notifies Chromatic to pause the animations when they finish at a global level
     // This is because Chromatic doesn't show the cover image when the animation starts with opacity 0
     pauseAnimationAtEnd: true,
-    // Set the viewports that Chromatic tests with globally.
+    // Set the viewport widths that Chromatic tests with globally.
     viewports: [400, 1200]
   },
 };
