f7e07a4c66bd57ba3c277f985f64e997fef892b1
Jeppe Krogh <kjej@aarhus.dk>
2023-03-10 16:25:47 +0100

    adding styling component, including storybook preview

---
+++ src/stories/Library/favorites-list-material-component/favorites-list-material-component.stories.tsx
@@ -0,0 +1,49 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import FavoritesListMaterialComponent from "./favorites-list-material-component";
+
+export default {
+  title: "Library / Favorites list material-component",
+  component: FavoritesListMaterialComponent,
+  argTypes: {
+    title: {
+      control: "text",
+      defaultValue: "Din favoritliste",
+    },
+    bright: {
+      control: { type: "boolean" },
+      defaultValue: true,
+    },
+    recommenderData: {
+      control: "object",
+      defaultValue: [
+        {
+          title: "Ella Fitzgerald",
+          authors: "Af Isabel Sánchez Vegara",
+          filled: true,
+        },
+        {
+          title: "Ella Fitzgerald",
+          authors: "Af Isabel Sánchez Vegara",
+          filled: true,
+        },
+        {
+          title: "Ella Fitzgerald",
+          authors: "Af Isabel Sánchez Vegara",
+          filled: false,
+        },
+        {
+          title: "Ella Fitzgerald",
+          authors: "Af Isabel Sánchez Vegara",
+          filled: false,
+        },
+      ],
+    },
+  },
+} as ComponentMeta<typeof FavoritesListMaterialComponent>;
+
+const Template: ComponentStory<typeof FavoritesListMaterialComponent> = (
+  args
+) => <FavoritesListMaterialComponent {...args} />;
+
+export const FavoritesList = Template.bind({});
+FavoritesList.args = {};
