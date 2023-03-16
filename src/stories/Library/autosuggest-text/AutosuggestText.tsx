93e3708c74e98fe3aa5a0a7285cc4fa977da9433
Adam Antal <adoantal@gmail.com>
2023-03-16 11:39:44 +0100

    Change AutosuggestText structure so it only has 1 <ul> & 1 level of <li>
    Because the autosuggest serves the role of a combobox it has to have
    a specific structure.
    https://www.w3.org/TR/wai-aria-1.1/#option
    https://www.w3.org/TR/wai-aria-1.1/#listbox
    https://www.w3.org/Translations/WCAG21-da/#parsing

--- src/stories/Library/autosuggest-text/AutosuggestText.tsx
+++ src/stories/Library/autosuggest-text/AutosuggestText.tsx
@@ -7,22 +7,18 @@ export const AutosuggestText = (props: AutosuggestTextProps) => {
   const { items, categoryText } = props;
   return (
     <ul className="autosuggest pb-16">
-      <li>
-        <ul>
-          {items.map((item) => {
-            return (
-              <li className="autosuggest__text text-body-medium-regular px-24">
-                {item}
-                {categoryText && (
-                  <div className="boxed-text text-tags noselect ml-8">
-                    {categoryText}
-                  </div>
-                )}
-              </li>
-            );
-          })}
-        </ul>
-      </li>
+      {items.map((item) => {
+        return (
+          <li className="autosuggest__text-item text-body-medium-regular px-24">
+            <p className="autosuggest__text text-body-medium-regular">{item}</p>
+            {categoryText && (
+              <div className="boxed-text text-tags noselect ml-8">
+                {categoryText}
+              </div>
+            )}
+          </li>
+        );
+      })}
     </ul>
   );
 };
