c776794241b6a5bc1df8f3bf65cb331b0c7464b1
Adam Antal <adoantal@gmail.com>
2023-03-23 11:13:00 +0100

    Simplify autosuggest-material story(remove unnecessary header component)
    We now have a separate story for autosuggest as a whole - under Blocks /
    / Autosuggest where this code is present. We would like to remove as
    much of this duplication as possible.

--- src/stories/Library/autosuggest-material/AutosuggestMaterial.stories.tsx
+++ src/stories/Library/autosuggest-material/AutosuggestMaterial.stories.tsx
@@ -1,11 +1,30 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { withDesign } from "storybook-addon-designs";
-import { AutosuggestText } from "../autosuggest-text/AutosuggestText";
 import {
   AutosuggestMaterial,
   AutosuggestMaterialProps,
 } from "./AutosuggestMaterial";
-import materialSuggestions from "./material-suggestions";
+
+const autosuggestMaterialSuggestions = [
+  {
+    cover: "images/book_cover_1.jpg",
+    title: "De uadskillelige",
+    author: "Simone De Beauvoir",
+    year: "1954",
+  },
+  {
+    cover: "images/book_cover_2.jpg",
+    title: "Den lille bog om dansk design",
+    author: "Marie Hugsted",
+    year: "2018",
+  },
+  {
+    cover: "images/book_cover_3.jpg",
+    title: "Audrey Hepburn",
+    author: "Maria Isabel Sanchez Vegara",
+    year: "2018",
+  },
+];
 
 export default {
   title: "Library / Autosuggest - Material",
@@ -14,7 +33,7 @@ export default {
   argTypes: {
     materialSuggestions: {
       name: "Material suggestions",
-      defaultValue: materialSuggestions,
+      defaultValue: autosuggestMaterialSuggestions,
       control: { type: "array" },
     },
   },
@@ -29,41 +48,24 @@ export default {
 const Template: ComponentStory<typeof AutosuggestMaterial> = (
   args: AutosuggestMaterialProps
 ) => (
-  // Apart from AutosuggestMaterial, everything else is here just for the story
-  // context. AutosuggestMaterial style is directly dependent on the header,
-  // search field, and AutosuggestText styling.
-  // If you find out this context no longer represents reality, please adjust it.
-  <header className="header" style={{ height: "144px" }}>
-    <div className="header__logo-desktop">
-      <p className="text-body-medium-regular">Context</p>
-    </div>
-    <div className="header__menu">
-      <nav className="header__menu-first">
-        <p className="text-body-medium-regular">Context</p>
-      </nav>
-      <div className="header__menu-second">
-        <div className="header__menu-search">
-          <input
-            className="header__menu-search-input text-body-medium-regular"
-            type="text"
-            placeholder="This field is here just for context."
-          />
-          <img
-            className="header__menu-search-icon"
-            src="icons/basic/icon-search.svg"
-            alt="search icon"
-          />
-          <ul className="autosuggest pb-16">
-            <AutosuggestText textSuggestions={["I am a contextual item"]} />
-            <AutosuggestMaterial {...args} />
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div className="header__clock">
-      <p className="text-body-medium-regular">Context</p>
-    </div>
-  </header>
+  <div className="header__menu-search">
+    <ul className="autosuggest pb-16">
+      <AutosuggestMaterial {...args} />
+    </ul>
+  </div>
 );
 
-export const MaterialSuggestion = Template.bind({});
+export const Default = Template.bind({});
+
+export const TwoItems = Template.bind({});
+TwoItems.args = {
+  materialSuggestions: [
+    autosuggestMaterialSuggestions[0],
+    autosuggestMaterialSuggestions[1],
+  ],
+};
+
+export const SingleItem = Template.bind({});
+SingleItem.args = {
+  materialSuggestions: [autosuggestMaterialSuggestions[0]],
+};
