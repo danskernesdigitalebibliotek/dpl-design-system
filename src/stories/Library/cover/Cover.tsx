da5d8af5330d11ba662de83a6b621598d54a3f2a
Jeppe Krogh <kjej@aarhus.dk>
2023-04-11 13:21:01 +0200

    added generic accessible content for covers

--- src/stories/Library/cover/Cover.tsx
+++ src/stories/Library/cover/Cover.tsx
@@ -28,7 +28,13 @@ const Cover: FC<CoverProps> = ({
     // Images inside links must have an non-empty alt text to meet accessibility requirements.
     // Only render the cover as a link if we have both an url and a description.
     return (
-      <a className={classes.wrapper} href={coverUrl}>
+      <a
+        className={classes.wrapper}
+        href={coverUrl}
+        aria-label="cover aria label"
+        aria-labelledby="cover labelled by"
+        title="cover title text"
+      >
         <CoverImage
           setImageLoaded={() => setImageLoaded(true)}
           src={src}
