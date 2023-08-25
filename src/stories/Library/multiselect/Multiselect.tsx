7285bee4d748f40ae4fa2f743eda6e04bdbc28f7
Adam Antal <adoantal@gmail.com>
2023-08-25 09:08:31 +0200

    Introduce multiselect dropdown with styling & story
    As a part of this, minor changes were made to the checkbox component
    so that it can receive custom styling.

---
+++ src/stories/Library/multiselect/Multiselect.tsx
@@ -0,0 +1,46 @@
+import { Checkbox } from "../Forms/checkbox/Checkbox";
+
+export type MultiselectProps = {
+  options: string[];
+  isOpen: boolean;
+};
+
+export const Multiselect: React.FC<MultiselectProps> = ({
+  options,
+  isOpen,
+}) => {
+  return (
+    <div className="multiselect">
+      <div className="multiselect__selected">
+        Item 1, Item 2, Item 3, Item 4
+      </div>
+      <div className="multiselect__opener">
+        <img
+          className="multiselect__icon"
+          src="icons/collection/ExpandMore.svg"
+          alt=""
+        />
+      </div>
+      {isOpen && (
+        <ul className="multiselect__options">
+          {options.map((item, index) => (
+            <li
+              className={`multiselect__option ${
+                index === 0 ? "multiselect__option--highlighted" : ""
+              }`}
+              key={`${item}${index}`}
+            >
+              {item}
+              <Checkbox
+                hiddenLabel
+                isChecked
+                ariaLabel={`checkbox ${item}`}
+                classNames="multiselect__checkbox"
+              />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
