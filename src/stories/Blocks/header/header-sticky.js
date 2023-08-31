85dd11f81de075dae28ee4c54de1e2421182e90b
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-08-31 11:32:18 +0200

    Remove deprecated up/down classes
    Since we manipulate with the style attribute of the header directly

--- src/stories/Blocks/header/header-sticky.js
+++ src/stories/Blocks/header/header-sticky.js
@@ -7,12 +7,6 @@ window.addEventListener("DOMContentLoaded", () => {
   const headerDownTopPositionOffset = 5;
   const waitPxBeforeScroll = 5;
 
-  // If we're at the top of the page, make sure the header class is "up".
-  if (window.scrollY === 0) {
-    document.querySelector("header").classList.remove("down");
-    document.querySelector("header").classList.add("up");
-  }
-
   const updateScrollDirection = () => {
     const { scrollY } = window;
     // Detect if we are going up or down.
@@ -27,16 +21,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // Control header direction class.
     if (scrollDirection === "down") {
-      document.querySelector("header").classList.remove("up");
-      document.querySelector("header").classList.add("down");
       const headerHeight = document.querySelector("header").offsetHeight;
       const headerDownTopPosition = headerHeight + headerDownTopPositionOffset;
       document.querySelector(
         "header"
       ).style.top = `-${headerDownTopPosition}px`;
     } else {
-      document.querySelector("header").classList.remove("down");
-      document.querySelector("header").classList.add("up");
       document.querySelector(
         "header"
       ).style.top = `${initialHeaderYposition}px`;
