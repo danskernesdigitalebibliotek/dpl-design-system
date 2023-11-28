183954b05aa831207c4f8af521d2a52993028d13
Kasper Birch <kasperbirch1@gmail.com>
2023-11-28 11:23:45 +0100

    Refactor `article-header` CSS Following Strategy Shift
    In alignment with our recent team decision, this commit moves away from utility classes towards an extended use of BEM classes.
    
    Additionally, the markup has been revised to improve semantic structure.

--- src/stories/Library/article-header/ArticleHeader.tsx
+++ src/stories/Library/article-header/ArticleHeader.tsx
@@ -5,7 +5,6 @@ import ArrowLink from "../links/arrow-link/ArrowLink";
 type ArticleHeaderProps = {
   title: string;
   subtitle: string;
-  library: string;
   author: string;
   date: string;
 };
@@ -13,21 +12,20 @@ type ArticleHeaderProps = {
 const ArticleHeader: FC<ArticleHeaderProps> = ({
   title,
   subtitle,
-  library,
   author,
   date,
 }) => {
   return (
     <header className="article-header">
       <ArrowLink label="Go back" className="article-header__back-link" />
-      <h1 className="text-header-h1 article-header__title">{title}</h1>
-      <p className="text-subtitle article-header__subtitle">{subtitle}</p>
-      <p className="article-header__info text-body-small-regular">
-        <span>{library}</span>
-        <a href="/">
-          <span className="link-tag text-body-small-regular">{author}</span>
+      <h1 className="article-header__title">{title}</h1>
+      <p className="article-header__subtitle">{subtitle}</p>
+      <p className="article-header__info">
+        Af
+        <a href="/" className="article-header__author">
+          {author}
         </a>
-        <span className="article-header__date">{date}</span>
+        <time className="article-header__date">{date}</time>
       </p>
       <RowButtons labels={["Netmedier", "Licenser", "This is hiddden"]} />
     </header>
