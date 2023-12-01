958b023d80852a3d3c63c250efa6aed0a9c4860f
Kasper Birch <kasperbirch1@gmail.com>
2023-12-01 14:36:19 +0100

    Add `image` to event props
    Enabling user experimentation with various images.

--- src/stories/Blocks/event-page/EventPage.stories.tsx
+++ src/stories/Blocks/event-page/EventPage.stories.tsx
@@ -15,6 +15,11 @@ export default {
       defaultValue: "06 Dec - 12 Jan 2022",
       type: "string",
     },
+    image: {
+      defaultValue:
+        "https://plus.unsplash.com/premium_photo-1696886122527-e4303b76aa8f?q=80&w=5156&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      type: "string",
+    },
   },
   parameters: {
     design: {
