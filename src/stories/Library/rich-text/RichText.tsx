b7cd931071bc5a8b33c71853b25b9cdf14c11d92
Benjamin Rasmussen <benjamin@reload.dk>
2023-12-05 23:27:42 +0100

    Rich text.

---
+++ src/stories/Library/rich-text/RichText.tsx
@@ -0,0 +1,44 @@
+export interface RichTextProps {
+  text: string;
+}
+
+export const RichText = () => {
+  return (
+    <div className="rich-text">
+      <h2>Tad survive ensnare joy mistake courtesy Bagshot Row.</h2>
+      <p>
+        Ligulas step drops both? You shall not pass! Tender respectable success
+        Valar impressive unfriendly bloom scraped? Branch hey-diddle-diddle pony
+        troublell sleeping during jump Narsil.
+      </p>
+      <h3>North valor overflowing sort Iáve mister kingly money?</h3>
+      <p>
+        Curse you and all the halflings! Deserted anytime Lake-town burned caves
+        balls. Smoked lthilien forbids Thrain?
+      </p>
+      <ul>
+        <li>Adamant.</li>
+        <li>Southfarthing!</li>
+        <li>Witch-king.</li>
+        <li>Precious.</li>
+        <li>Gaffers!</li>
+      </ul>
+      <h3>
+        Narsil enjoying shattered bigger leaderless retrieve dreamed dwarf.
+      </h3>
+      <p>
+        Ravens wonder wanted runs me crawl gaining lots faster! Khazad-dum
+        surprise baby season ranks. I bid you all a very fond farewell.
+      </p>
+      <ol>
+        <li>Narsil.</li>
+        <li>Elros.</li>
+        <li>Arwen Evenstar.</li>
+        <li>Maggots?</li>
+        <li>Bagginses?</li>
+      </ol>
+    </div>
+  );
+};
+
+export default RichText;
