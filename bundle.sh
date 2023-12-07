a62ed27994d2f0c9c1dc8c4b702456753a4461ad
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2023-12-07 18:07:04 +0100

    Debug design build 1806

--- bundle.sh
+++ bundle.sh
@@ -6,5 +6,5 @@ cp -r ./public/icons ./build/icons
 cp -r ./src/styles/css ./build/css
 cp -r ./src/styles/fonts ./build/fonts
 find ./src -name "*.js" -exec cp {} ./build/js \;
-echo "1730" > ./build/version-debug.txt
+echo "1806" > ./build/version-debug.txt
 zip -r dist.zip build/
