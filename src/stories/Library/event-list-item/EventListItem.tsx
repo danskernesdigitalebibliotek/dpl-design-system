d9b169e8e29572fb2aef7fb7e3eacd5b3c127567
LasseStaus <l.stausgaard@gmail.com>
2024-01-04 17:02:28 +0100

    Added datetime attributes to eventListItem.tsx
    Added both a value for the event date containing only the date, and another for with an ISO formatted date and time for the event time.
    
    DDFFORM-59

--- src/stories/Library/event-list-item/EventListItem.tsx
+++ src/stories/Library/event-list-item/EventListItem.tsx
@@ -1,4 +1,3 @@
-// EventListItem.tsx
 import { ReactComponent as ArrowSmallRight } from "../Arrows/icon-arrow-ui/icon-arrow-ui-small-right.svg";
 import { Tag } from "../tag/Tag";
 
@@ -25,6 +24,11 @@ export const EventListItem: React.FC<EventListItemProps> = ({
   price,
   href,
 }) => {
+  // Hardcoded placeholders for datetime
+  // These are calculated in the corresponding template in the CMS
+  const placeholderDate = "2023-03-10"; // ISO format date
+  const placeholderDateTime = "2023-03-10T15:00"; // ISO format date and time
+
   return (
     <a href={href} className="event-list-item arrow__hover--right-small">
       <div className="event-list-item__image-container">
@@ -34,14 +38,21 @@ export const EventListItem: React.FC<EventListItemProps> = ({
         <Tag hasBackground className="event-list-item__tag">
           {tagText}
         </Tag>
-        <p className="event-list-item__date">{date}</p>
+        <time className="event-list-item__date" dateTime={placeholderDate}>
+          {date}
+        </time>
         <h2 className="event-list-item__title">{title}</h2>
         <p className="event-list-item__description">{description}</p>
         <div className="event-list-item__location-wrapper">
           <p className="event-list-item__location">{location}</p>
         </div>
         <div className="event-list-item__schedule">
-          <time className="event-list-item__time">{time}</time>
+          <time
+            className="event-list-item__time"
+            dateTime={placeholderDateTime}
+          >
+            {time}
+          </time>
           <p className="event-list-item__pricing">{price}</p>
         </div>
       </div>
