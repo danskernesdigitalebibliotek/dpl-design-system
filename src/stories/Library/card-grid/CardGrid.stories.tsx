6e17f20da9b58127acef0a842b399223287d1adb
Kasper Birch <kasperbirch1@gmail.com>
2024-02-01 15:28:18 +0100

    Add `Recommendation` and `CardGrid` to `ArticleParagraphs`

--- src/stories/Library/card-grid/CardGrid.stories.tsx
+++ src/stories/Library/card-grid/CardGrid.stories.tsx
@@ -8,7 +8,7 @@ const imageUrl =
   "https://images.unsplash.com/photo-1568667256549-094345857637?q=80&w=2815&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 const image = <ImageCredited src={imageUrl} />;
 
-const card = (
+export const card = (
   <Card
     title="Bøger som har gjort en forskel for romanens udvikling"
     typeTag="Nyhed"
@@ -17,7 +17,7 @@ const card = (
   />
 );
 
-const cardNoImage = (
+export const cardNoImage = (
   <Card
     title="Fars legestue, hver onsdag"
     typeTag="Arrangement"
--- src/stories/Library/paragraphs/Paragraphs.tsx
+++ src/stories/Library/paragraphs/Paragraphs.tsx
@@ -1,5 +1,8 @@
+import CardGrid from "../card-grid/CardGrid";
+import { card, cardNoImage } from "../card-grid/CardGrid.stories";
 import { LinkWithIcon } from "../link-with-icon/LinkWithIcon";
 import Paragraph from "../paragraph/Paragraph";
+import { Recommendation } from "../recommendation/Recommendation";
 import { RichText, RichTextEvent } from "../rich-text/RichText";
 import VideoEmbed from "../video-embed/VideoEmbed";
 
@@ -34,6 +37,43 @@ export const ArticleParagraphs = () => {
 
       <Paragraph modyfier="links">
         <LinkWithIcon href="/" linkText="External Link" linkType="external" />
+      </Paragraph>
+
+      <Paragraph modyfier="recommendation">
+        <Recommendation
+          title="Børnenes Naturhobbybog"
+          description="Naturen er fyldt med alle mulige ting fx blade, blomster, sten osv., der kan bruges til at lave en forskellige kreative ting af. Her er inspiration at hente til små og store hobbyprojekter."
+          href="#"
+          recommendedMaterialProps={{
+            author: "Pia Deges (2019)",
+            description: "Børnenes Naturhobbybog",
+            src: "images/book_cover_6.jpg",
+            alt: "book cover",
+            favoriteFill: false,
+            coverUrl: "#",
+          }}
+        />
+      </Paragraph>
+
+      <Paragraph modyfier="recommendation">
+        <Recommendation
+          positionImageRight
+          title="Børnenes Naturhobbybog"
+          description="Naturen er fyldt med alle mulige ting fx blade, blomster, sten osv., der kan bruges til at lave en forskellige kreative ting af. Her er inspiration at hente til små og store hobbyprojekter."
+          href="#"
+          recommendedMaterialProps={{
+            author: "Pia Deges (2019)",
+            description: "Børnenes Naturhobbybog",
+            src: "images/book_cover_6.jpg",
+            alt: "book cover",
+            favoriteFill: false,
+            coverUrl: "#",
+          }}
+        />
+      </Paragraph>
+
+      <Paragraph modyfier="card_grid">
+        <CardGrid items={[card, cardNoImage, card, card, card, cardNoImage]} />
       </Paragraph>
     </section>
   );
