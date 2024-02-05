a0ee83bfc398574fbe1e1f8c7b75f1094305fb7d
Adam Antal <adoantal@gmail.com>
2024-02-05 11:05:54 +0100

    Make Reservation List Item with a note have a red counter with a warning

--- src/stories/Blocks/reservation-page/ReservationListItem.tsx
+++ src/stories/Blocks/reservation-page/ReservationListItem.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { Counter } from "../../Library/counter/Counter";
 import { ReactComponent as ArrowSmallRight } from "../../Library/Arrows/icon-arrow-ui/icon-arrow-ui-small-right.svg";
 
@@ -60,21 +61,34 @@ const ReservationListItem: React.FC<ReservationListItemProps> = ({
           </div>
           <div className="list-reservation__status">
             <div className="list-reservation__counter color-secondary-gray">
-              <Counter
-                isReady
-                label="Ready"
-                percentage={100}
-                status="info"
-                value={20}
-              />
+              {!withNote && (
+                <Counter
+                  isReady
+                  label="Ready"
+                  percentage={100}
+                  status="info"
+                  value={20}
+                />
+              )}
+              {withNote && (
+                <Counter
+                  label="Days"
+                  percentage={100}
+                  status="danger"
+                  value={0}
+                />
+              )}
             </div>
             <div>
               <div className="list-reservation__deadline">
                 <div
                   data-cy="info-label"
-                  className="status-label status-label--info"
+                  className={clsx("status-label", [
+                    { "status-label--info": !withNote },
+                    { "status-label--danger": withNote },
+                  ])}
                 >
-                  Pick up before 02-02-2024
+                  Pick up before xx-xx-xxxx
                 </div>
                 <p className="text-small-caption">Hovedbiblioteket</p>
                 <p className="text-small-caption">Reserveringshylde 74</p>
