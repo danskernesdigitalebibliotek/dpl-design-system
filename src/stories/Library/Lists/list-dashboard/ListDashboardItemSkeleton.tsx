c23e245225f882422cbe090dca5604e0e64e1ff4
Adam Antal <adoantal@gmail.com>
2024-02-08 13:01:38 +0100

    Introduce the ListDashboardItemSkeleton component and styling

---
+++ src/stories/Library/Lists/list-dashboard/ListDashboardItemSkeleton.tsx
@@ -0,0 +1,10 @@
+const ListDashboardItemSkeleton: React.FC = () => {
+  return (
+    <div className="list-dashboard ssc">
+      <div className="ssc-circle" />
+      <div className="ssc-head-line w-40" />
+    </div>
+  );
+};
+
+export default ListDashboardItemSkeleton;
