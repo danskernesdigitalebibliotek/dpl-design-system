24d05cadf12e1b6c3b1bba349c0f602462c77fa3
LasseStaus <l.stausgaard@gmail.com>
2024-02-21 14:34:00 +0100

    Create material grid component and story
    DDFFORM-313

---
+++ src/stories/Library/material-grid/MaterialGrid.stories.tsx
@@ -0,0 +1,47 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { MaterialGrid } from "./MaterialGrid";
+import MaterialGridData from "./MaterialGridData";
+
+export default {
+  title: "Library/ Material Grid",
+  component: MaterialGrid,
+  argTypes: {
+    title: {
+      control: "text",
+      description: "Title of the recommendation",
+    },
+    selectedAmountOfMaterialsForDisplay: {
+      control: {
+        type: "select",
+        options: [4, 8, 12, 16, 20, 24, 28, 32],
+      },
+      description: "Amount of materials to show",
+    },
+    materials: {
+      control: "object",
+      description: "Recommended materials",
+    },
+  },
+  args: {
+    title: "Recommended materials",
+    selectedAmountOfMaterialsForDisplay: 4,
+    materials: MaterialGridData,
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/Zx9GrkFA3l4ISvyZD2q0Qi/Designsystem?type=design&node-id=446-6954&mode=design&t=IkusOClO6YuhuPrb-4",
+    },
+  },
+} as ComponentMeta<typeof MaterialGrid>;
+
+const Template: ComponentStory<typeof MaterialGrid> = (args) => (
+  <MaterialGrid {...args} />
+);
+
+export const Default = Template.bind({});
+
+export const GridWithMoreMaterials = Template.bind({});
+GridWithMoreMaterials.args = {
+  selectedAmountOfMaterialsForDisplay: 8,
+};
---
+++ src/stories/Library/material-grid/MaterialGrid.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import {
+  RecommendedMaterial,
+  RecommendedMaterialProps,
+} from "../recommended-material/RecommendedMaterial";
+
+export type MaterialGridProps = {
+  title: string;
+  selectedAmountOfMaterialsForDisplay: 4 | 8 | 12 | 16 | 20 | 24 | 28 | 32;
+  materials: RecommendedMaterialProps[];
+};
+
+export const MaterialGrid: React.FC<MaterialGridProps> = ({
+  title,
+  selectedAmountOfMaterialsForDisplay,
+  materials,
+}) => {
+  // The JS code here is just an example of how this works.
+  // The proper implementation is in the react repo.
+  const initialMaximumDisplay = 4;
+  const initialMaterialsToDisplay =
+    selectedAmountOfMaterialsForDisplay > initialMaximumDisplay
+      ? initialMaximumDisplay
+      : selectedAmountOfMaterialsForDisplay;
+
+  const [showAllMaterials, setShowAllMaterials] = useState(false);
+  const [currentMaterialsDisplayed, setCurrentMaterialsDisplayed] = useState(
+    initialMaterialsToDisplay
+  );
+
+  const moreMaterialsThanInitialMaximum =
+    selectedAmountOfMaterialsForDisplay > initialMaximumDisplay;
+
+  function handleSetShowAllMaterials() {
+    setShowAllMaterials(true);
+    setCurrentMaterialsDisplayed(selectedAmountOfMaterialsForDisplay);
+  }
+
+  return (
+    <div className="material-grid">
+      <h2 className="material-grid__title">{title}</h2>
+      <ul className="material-grid__items">
+        {materials
+          .slice(0, currentMaterialsDisplayed)
+          .map((material, index) => (
+            <li key={index}>
+              <RecommendedMaterial {...material} />
+            </li>
+          ))}
+      </ul>
+      {moreMaterialsThanInitialMaximum && !showAllMaterials && (
+        <button
+          className="material-grid__show-more btn-primary btn-outline btn-medium"
+          data-show-more
+          aria-expanded={showAllMaterials ? "true" : "false"}
+          aria-controls="material-grid__items"
+          onClick={() => handleSetShowAllMaterials()}
+        >
+          Se alle
+        </button>
+      )}
+    </div>
+  );
+};
