37677ccec9001f5899b24f25970b56276449cf0e
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2024-03-05 22:42:19 +0100

    Adding new error message story and styling
    Used both for Drupal and react errors.

---
+++ src/stories/Library/error-message/ErrorMessage.tsx
@@ -0,0 +1,25 @@
+import { ReactComponent as WarningIcon } from "../../../public/icons/basic/icon-warning.svg";
+
+export type ErrorMessageProps = {
+  message: string;
+};
+
+export const ErrorMessage = ({ message }: ErrorMessageProps) => {
+  return (
+    <div className="error-message">
+      <WarningIcon className="error-message__icon" />
+      <div className="error-message__description">
+        <div id="dpl-react-apps-error-messages">{message}</div>
+      </div>
+
+      <button
+        type="button"
+        aria-describedby="describemodal"
+        className="error-message__btn-close"
+        aria-label="close modal"
+      >
+        <img src="icons/collection/CloseLarge.svg" alt="close modal button" />
+      </button>
+    </div>
+  );
+};
