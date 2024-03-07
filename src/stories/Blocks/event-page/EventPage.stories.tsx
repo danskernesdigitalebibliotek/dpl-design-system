8c6fec9ff6976274e51deb8e40c6c6351cc15adb
Kasper Birch <kasperbirch1@gmail.com>
2024-03-07 17:21:20 +0100

    Add missing credits to event page story

--- src/stories/Blocks/event-page/EventPage.stories.tsx
+++ src/stories/Blocks/event-page/EventPage.stories.tsx
@@ -18,7 +18,11 @@ export default {
     },
     image: {
       defaultValue: (
-        <ImageCredited src="https://images.unsplash.com/photo-1531058020387-3be344556be6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8ZXZlbnR8fHx8fHwxNzAyOTEwMzE0&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" />
+        <ImageCredited
+          src="https://images.unsplash.com/photo-1531058020387-3be344556be6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8ZXZlbnR8fHx8fHwxNzAyOTEwMzE0&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+          description="Photo by Unsplash"
+          year="©2021"
+        />
       ),
     },
     placeholderText: {
