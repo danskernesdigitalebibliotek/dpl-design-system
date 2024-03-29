bd1886d05396aa1b552e3561f97e6e649badb1b3
Benjamin Rasmussen <benjamin@reload.dk>
2024-03-29 23:50:42 +0100

    Make sure hero visual respects 5/4. DDFFORM-360

--- src/stories/Blocks/event-page/EventPage.stories.tsx
+++ src/stories/Blocks/event-page/EventPage.stories.tsx
@@ -18,7 +18,11 @@ export default {
     },
     image: {
       defaultValue: (
-        <ImageCredited src="https://images.unsplash.com/photo-1531058020387-3be344556be6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8ZXZlbnR8fHx8fHwxNzAyOTEwMzE0&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080" />
+        <ImageCredited
+          description="Photo by Smith on Unsplash"
+          year="©2021"
+          src="https://images.unsplash.com/photo-1531058020387-3be344556be6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8ZXZlbnR8fHx8fHwxNzAyOTEwMzE0&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+        />
       ),
     },
     placeholderText: {
