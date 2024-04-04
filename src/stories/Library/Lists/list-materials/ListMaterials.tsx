623f61498e3b66bbf7b8e0f84713dad03308103f
Adam Antal <adoantal@gmail.com>
2024-04-04 11:48:37 +0200

    Change layout of list material cards on mobile vs desktop
    Utilizing the list-materials__status--mobile &
    list-materials__status--desktop classes.

--- src/stories/Library/Lists/list-materials/ListMaterials.tsx
+++ src/stories/Library/Lists/list-materials/ListMaterials.tsx
@@ -1,6 +1,7 @@
 import { useState, FC } from "react";
 import { Checkbox } from "../../Forms/checkbox/Checkbox";
 import { StatusLabel, StatusLabelProps } from "../../status-label/StatusLabel";
+import { ReactComponent as ArrowSmallRight } from "../../Arrows/icon-arrow-ui/icon-arrow-ui-small-right.svg";
 
 export type ListMaterialsProps = {
   title: string;
@@ -42,20 +43,33 @@ export const ListMaterials: FC<ListMaterialsProps> = ({
             <div className="list-materials__content-status">
               <StatusLabel {...statusMaterialType} />
             </div>
+            <div className="status-label status-label--neutral hide-on-desktop ml-16">
+              Afleveres 27-04-2023
+            </div>
             <p className="list-materials__content__header mt-8">{title}</p>
             <p className="text-small-caption">
               {author} ({year})
             </p>
-          </div>
-          <div className="list-materials__status">
-            <div>
-              <div className="status-label status-label--neutral">
-                Afleveres 27-04-2023
-              </div>
+            <div className="list-materials__status list-materials__status--mobile">
               {statusMessage && (
-                <span className="list-reservation__note">{statusMessage}</span>
+                <span className="list-materials__status__note-mobile">
+                  {statusMessage}
+                </span>
               )}
             </div>
+          </div>
+          <div className="list-materials__status list-materials__status--desktop">
+            <div className="status-label status-label--neutral">
+              Afleveres 27-04-2023
+            </div>
+            {statusMessage && (
+              <span className="list-materials__status__note-desktop">
+                {statusMessage}
+              </span>
+            )}
+          </div>
+          <div className="list-materials__arrow arrow-button">
+            <ArrowSmallRight />
           </div>
         </div>
       </li>
@@ -72,25 +86,24 @@ export const ListMaterials: FC<ListMaterialsProps> = ({
           <div className="list-materials__content">
             <div className="list-materials__content-status">
               <div className="status-label status-label--outline">Bog</div>
+              <div className="status-label status-label--neutral hide-on-desktop ml-16">
+                Afleveres 27-04-2023
+              </div>
             </div>
             <p className="list-materials__content__header mt-8">{title}</p>
             <p className="text-small-caption">
               {author} ({year})
             </p>
           </div>
-          <div className="list-materials__status">
+          <div className="list-materials__status list-materials__status--desktop">
             <div>
               <div className="status-label status-label--neutral">
                 Afleveres 27-04-2023
               </div>
-              <button
-                type="button"
-                className="list-reservation__note"
-                aria-label="Go to Alt om håndarbejdes strikkemagasin material details"
-              >
-                Go to material details
-              </button>
             </div>
+          </div>
+          <div className="list-materials__arrow arrow-button">
+            <ArrowSmallRight />
           </div>
         </div>
       </li>
