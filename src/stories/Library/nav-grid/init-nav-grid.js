18af12e134d74908abd84f91f603403ead2e11f0
Kasper Birch <kasperbirch1@gmail.com>
2024-05-15 13:32:56 +0200

    Enhance `nav-grid` functionality to focus on next item after "show more" click

--- src/stories/Library/nav-grid/init-nav-grid.js
+++ src/stories/Library/nav-grid/init-nav-grid.js
@@ -13,6 +13,15 @@ window.addEventListener("load", () => {
 
     button.addEventListener("click", () => {
       grid.classList.remove("nav-grid--folded");
+
+      // The 7th item is the first one that is initially hidden.
+      // For more details, refer to the ".nav-grid--folded" class in design-system src/stories/Library/nav-grid/nav-grid.scss
+      const seventhNavGridItem = grid.querySelector(
+        ".nav-grid__item:nth-child(7)"
+      );
+      if (seventhNavGridItem) {
+        seventhNavGridItem.querySelector("a").focus();
+      }
     });
   });
 });
