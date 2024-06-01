a0f44b356518ba1ce2dd199a1de46afe82585f3b
Kasper Birch <kasperbirch1@gmail.com>
2024-06-01 14:34:51 +0200

    Add new `banner` component
    This component contains a required link and title. It can be used with a description and a background image to enhance visual appeal.
    
    The title field is a `ReactNode` where the `<u>` (underlined) tag has a custom underline SVG placed beneath it.

---
+++ src/stories/Library/banner/Banner.stories.tsx
@@ -0,0 +1,53 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { withDesign } from "storybook-addon-designs";
+import Banner from "./Banner";
+import ImageCredited from "../image-credited/ImageCredited";
+
+export default {
+  title: "Library / Banner",
+  component: Banner,
+  decorators: [withDesign],
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/Zx9GrkFA3l4ISvyZD2q0Qi/Designsystem?node-id=446-6957&t=IWAOniAbcjV2y2Hf-4",
+    },
+  },
+  argTypes: {
+    image: {
+      defaultValue: (
+        <ImageCredited
+          src="https://images.unsplash.com/photo-1531058020387-3be344556be6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8ZXZlbnR8fHx8fHwxNzAyOTEwMzE0&ixlib=rb-4.0.3&q=8
+0&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+        />
+      ),
+    },
+    title: {
+      name: "Title",
+      defaultValue: "Hvad skal jeg <u>høre?</u>",
+      control: { type: "text" },
+    },
+    description: {
+      name: "Description",
+      defaultValue:
+        "Om du er dedikeret musiknørd eller moderat musikinteresseret, så er dette siden til dig. Her kan du finde anbefalinger, digitale musikmagasiner, nyheder, musiklitteratur og meget mere.",
+      control: { type: "text" },
+    },
+  },
+} as ComponentMeta<typeof Banner>;
+
+const Template: ComponentStory<typeof Banner> = (args) => <Banner {...args} />;
+
+export const Default = Template.bind({});
+
+export const NoImage = Template.bind({});
+NoImage.args = {
+  title: "Title <u>uden</u> billede",
+  image: undefined,
+};
+
+export const NoDescription = Template.bind({});
+NoDescription.args = {
+  title: "Banner <u>uden</u> beskrivelse",
+  description: undefined,
+};
---
+++ src/stories/Library/banner/Banner.tsx
@@ -0,0 +1,40 @@
+import { FC, ReactNode } from "react";
+import clsx from "clsx";
+import MediaContainer from "../media-container/MediaContainer";
+import { ReactComponent as ArrowLargeRight } from "../Arrows/icon-arrow-ui/icon-arrow-ui-large-right.svg";
+
+type BannerType = {
+  title: string;
+  image?: ReactNode;
+  description?: string;
+};
+
+const Banner: FC<BannerType> = ({ image, title, description }) => {
+  return (
+    <a href="#" className="banner">
+      {image && (
+        <div className="banner-visual">
+          <MediaContainer media={image} />
+        </div>
+      )}
+      <div
+        className={clsx("banner-content arrow__hover--right-large", {
+          "banner-content--no-image": !image,
+        })}
+      >
+        <h2
+          className="banner-content__title"
+          // We need to be able to replicate our WYSIWYG field in Drupal that makes it possible to underline (<u>) words.
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: title }}
+        />
+        {description && (
+          <p className="banner-content__description">{description}</p>
+        )}
+        <ArrowLargeRight />
+      </div>
+    </a>
+  );
+};
+
+export default Banner;
