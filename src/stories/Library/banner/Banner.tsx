e2ea0db622516ec1bf5c07a89b8bf10e36420c5c
Kasper Birch <kasperbirch1@gmail.com>
2024-06-13 15:31:07 +0200

    Enhance `banner` layout with dynamic spacing
    Replaced the fixed `margin-left` pixels in `banner-content` with a new layout wrapper `banner__spacing` for dynamic spacing.

--- src/stories/Library/banner/Banner.tsx
+++ src/stories/Library/banner/Banner.tsx
@@ -17,21 +17,23 @@ const Banner: FC<BannerType> = ({ image, title, description }) => {
           <MediaContainer media={image} />
         </div>
       )}
-      <div
-        className={clsx("banner-content arrow__hover--right-large", {
-          "banner-content--no-image": !image,
-        })}
-      >
-        <h2
-          className="banner-content__title"
-          // We need to be able to replicate our WYSIWYG field in Drupal that makes it possible to underline (<u>) words.
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: title }}
-        />
-        {description && (
-          <p className="banner-content__description">{description}</p>
-        )}
-        <ArrowLargeRight />
+      <div className="banner__spacing">
+        <div
+          className={clsx("banner-content arrow__hover--right-large", {
+            "banner-content--no-image": !image,
+          })}
+        >
+          <h2
+            className="banner-content__title"
+            // We need to be able to replicate our WYSIWYG field in Drupal that makes it possible to underline (<u>) words.
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: title }}
+          />
+          {description && (
+            <p className="banner-content__description">{description}</p>
+          )}
+          <ArrowLargeRight />
+        </div>
       </div>
     </a>
   );
