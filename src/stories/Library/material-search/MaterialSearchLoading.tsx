cb103514d1def3c7f6722ebded56224fd32851af
LasseStaus <l.stausgaard@gmail.com>
2024-06-17 13:29:24 +0200

    Create MaterialSearch subcomponent MaterialSearchLoading
    DDFFORM-611

---
+++ src/stories/Library/material-search/MaterialSearchLoading.tsx
@@ -0,0 +1,20 @@
+import LoadingLogo from "../../../public/icons/logo/reload_logo_black.svg";
+
+interface ReloadLoadingIconProps {
+  loadingText: string;
+}
+
+const MaterialSearchLoading = ({ loadingText }: ReloadLoadingIconProps) => {
+  return (
+    <div className="material-search__loading">
+      <img
+        src={LoadingLogo}
+        alt=""
+        className="material-search__loading-spinner"
+      />
+      <span className="material-search__loading-text">{loadingText}</span>
+    </div>
+  );
+};
+
+export default MaterialSearchLoading;
