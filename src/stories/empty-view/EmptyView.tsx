d486b5d5de0c1937d5199dbd5a71497a399d00d4
Kasper Birch <kasperbirch1@gmail.com>
2024-09-09 11:08:23 +0200

    Implement `empty-view`
    This container is used in Drupal to display a message when no full-text search results are found.

---
+++ src/stories/empty-view/EmptyView.tsx
@@ -0,0 +1,10 @@
+const EmptyView = () => {
+  return (
+    <div className="empty-view">
+      Unfortunately, there were no results for your search. Try using a
+      different search term.
+    </div>
+  );
+};
+
+export default EmptyView;
