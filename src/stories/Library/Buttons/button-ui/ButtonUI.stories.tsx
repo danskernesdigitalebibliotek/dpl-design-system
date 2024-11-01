27b1d45f9892936dd172b7465ba47612b7477a1e
Jacob Pihl <jacob@reload.dk>
2024-11-01 14:23:42 +0100

    Fix ButtonUI story

--- src/stories/Library/Buttons/button-ui/ButtonUI.stories.tsx
+++ src/stories/Library/Buttons/button-ui/ButtonUI.stories.tsx
@@ -1,19 +1,27 @@
 import { StoryFn, Meta } from "@storybook/react";
 
-import { ButtonUI } from "./ButtonUI";
+import { ButtonUI, ButtonUIProps } from "./ButtonUI";
 
 export default {
   title: "Library / Buttons / ButtonUI",
   component: ButtonUI,
   argTypes: {
+    content: {
+      control: "object",
+    },
+    ariaLabel: { control: "text" },
     label: { control: "text" },
     disabled: { control: "boolean" },
     collapsible: { control: "boolean" },
-    size: { control: "select", options: ["small", "medium", "large"] },
-    variant: { control: "select", options: ["outline", "filled"] },
+    size: { control: "select" },
+    variant: { control: "select" },
   },
   args: {
-    label: "Se hele kalenderen",
+    content: {
+      kind: "LABEL",
+      label: "Se hele kalenderen",
+    },
+    ariaLabel: "Calendar button",
     disabled: false,
     collapsible: false,
     size: "medium",
@@ -28,7 +36,9 @@ export default {
   },
 } as Meta<typeof ButtonUI>;
 
-const Template: StoryFn<typeof ButtonUI> = (args) => <ButtonUI {...args} />;
+const Template: StoryFn<typeof ButtonUI> = (args: ButtonUIProps) => (
+  <ButtonUI {...args} />
+);
 
 export const Default = Template.bind({});
 Default.args = {};
