28c5f7af3c42989b48934cea24b5acc455bbd486
LasseStaus <l.stausgaard@gmail.com>
2025-04-30 09:42:41 +0200

    Add MaterialGridRelated with CSS & Story
    DDFSAL-1
    DDFSAL-4

---
+++ src/stories/Library/material-grid-related/material-grid-related.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryFn } from "@storybook/react";
+import MaterialGridRelated from "./material-grid-related";
+
+export default {
+  title: "Library/Material Grid Related",
+  component: MaterialGridRelated,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/Zx9GrkFA3l4ISvyZD2q0Qi/Designsystem?type=design&node-id=446-6954&mode=design&t=IkusOClO6YuhuPrb-4",
+    },
+  },
+} as Meta<typeof MaterialGridRelated>;
+
+const Template: StoryFn<typeof MaterialGridRelated> = () => (
+  <MaterialGridRelated />
+);
+
+export const Default = Template.bind({});
