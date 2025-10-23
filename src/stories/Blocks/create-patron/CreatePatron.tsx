771b4f8da770410880626f4ccff2883329562380
thomasgross <thomasgrossrasmussen@gmail.com>
2025-10-23 15:56:11 +0200

    feat: setup styles for new create patron page

--- src/stories/Blocks/create-patron/CreatePatron.tsx
+++ src/stories/Blocks/create-patron/CreatePatron.tsx
@@ -1,13 +1,22 @@
 import { Button } from "../../Library/Buttons/button/Button";
+import DawaInput from "../../Library/Forms/dawa-input/DawaInput";
 import DoubleInputRow from "../../Library/Forms/input/DoubleInputRow";
+import Dialog from "../../Library/dialog/Dialog";
+import useDialog from "../../Library/dialog/useDialog";
 import { Dropdown } from "../../Library/dropdown/Dropdown";
 import { Links } from "../../Library/links/Links";
+import LibrarySelect from "../library-select/LibrarySelect";
+import { ReactComponent as LocationSvg } from "../../../public/icons/collection/Location.svg";
+import { ReactComponent as WarningSvg } from "../../../public/icons/basic/icon-warning.svg";
 
 export interface CreatePatronProps {
   headline: string;
 }
 
 const CreatePatron: React.FC<CreatePatronProps> = ({ headline }) => {
+  const { dialogContent, openDialogWithContent, closeDialog, dialogRef } =
+    useDialog();
+
   return (
     <div className="create-patron-page">
       <h1 className="create-patron-page__title">{headline}</h1>
@@ -40,6 +49,88 @@ const CreatePatron: React.FC<CreatePatronProps> = ({ headline }) => {
           }
           footnote="Choose preferred library for pickup of your future reservations."
         />
+
+        <LibrarySelect
+          label="Choose library*"
+          id="library-select"
+          description="Select the library you want to borrow from."
+          validation="Please select a library*"
+        />
+
+        <LibrarySelect
+          label="Choose library*"
+          id="library-select"
+          selectedLibrary={{
+            name: "Blågårdens Bibliotek",
+            street: "Blågårds Plads 5",
+            city: "2200 København N",
+          }}
+          description="Select the library you want to borrow from."
+          validation="Please select a library*"
+          onClickCallback={() => openDialogWithContent(<div>Library List</div>)}
+        />
+
+        <Dialog isSidebar closeDialog={closeDialog} ref={dialogRef}>
+          <div className="find-library-dialog">
+            <p className="find-library-dialog__title">
+              Find dit nærmeste bibliotek
+            </p>
+            <div className="find-library-dialog__location-group">
+              <DawaInput
+                id="address-input"
+                label="Indtast din adresse"
+                placeholder="Fx Torvegade 1, 1401 København K"
+                type="text"
+                classNames="dawa-input"
+              />
+              <button className="find-library-dialog__location">
+                <LocationSvg />
+                <p>Find nærmeste bibliotek ud fra din lokation</p>
+              </button>
+              <div className="find-library-dialog__error-message">
+                <WarningSvg />
+                <p>
+                  Årh nej. Der er sket en fejl i afstandsmåleren. Prøv at
+                  reloade siden eller indtast adressen forfra.
+                </p>
+              </div>
+            </div>
+            <div className="find-library-dialog__location-list">
+              <p className="find-library-dialog__location-list__title">
+                Vælg bibliotek
+              </p>
+              <button className="find-library-dialog__location-list__item">
+                <div>
+                  <p className="find-library-dialog__location-list__item__name">
+                    Biblioteket Rentemestervej
+                  </p>
+                  <div className="find-library-dialog__location-list__item__address">
+                    <p>Rentemestervej 1</p>
+                    <p>2400 København NV</p>
+                  </div>
+                </div>
+                <p className="find-library-dialog__location-list__item__distance">
+                  0,1 km
+                </p>
+              </button>
+              <button className="find-library-dialog__location-list__item find-library-dialog__location-list__item--selected">
+                <div>
+                  <p className="find-library-dialog__location-list__item__name">
+                    Biblioteket Rentemestervej
+                  </p>
+                  <div className="find-library-dialog__location-list__item__address">
+                    <p>Rentemestervej 1</p>
+                    <p>2400 København NV</p>
+                  </div>
+                </div>
+                <p className="find-library-dialog__location-list__item__distance">
+                  0,1 km
+                </p>
+              </button>
+            </div>
+          </div>
+        </Dialog>
+
         <div className="create-patron-page__buttons">
           <Button
             buttonType="none"
---
+++ src/stories/Library/Forms/dawa-input/DawaInput.tsx
@@ -0,0 +1,58 @@
+import clsx from "clsx";
+import Label from "../label/Label";
+
+export type InputProps = {
+  label: string;
+  type: "text" | "password";
+  id: string;
+  description?: string;
+  validation?: string;
+  classNames?: string;
+  labelClassName?: string;
+  placeholder?: string;
+};
+
+const DawaInput = (props: InputProps) => {
+  const {
+    label,
+    type,
+    id,
+    description,
+    validation,
+    classNames,
+    labelClassName,
+    placeholder,
+  } = props;
+  const invalid = validation ? "true" : "false";
+  return (
+    <div
+      className={clsx("dawa-input", classNames, {
+        "dawa-input--invalid": !!validation,
+      })}
+    >
+      <Label id={id} className={labelClassName}>
+        {label}
+      </Label>
+      <input
+        placeholder={placeholder}
+        aria-invalid={invalid}
+        aria-describedby={`description-${id}`}
+        aria-labelledby={validation ? `validation-${id}` : ""}
+        id={id}
+        type={type}
+      />
+      {description && (
+        <div className="dawa-input__description" id={`description-${id}`}>
+          {description}
+        </div>
+      )}
+      {validation && (
+        <div id={`validation-${id}`} className="dawa-input__validation">
+          {validation}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DawaInput;
